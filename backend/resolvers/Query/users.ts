1cf28638b72fe746e93ffac73f378883553d4860
Daniel Koch <daniel.koch@helsinki.fi>
2019-05-20 16:34:16 +0300

    Refactoring and mutation and query for CompletinRegistered

---
+++ backend/resolvers/Query/users.ts
@@ -0,0 +1,10 @@
+import { ForbiddenError } from "apollo-server-core";
+
+const users = async (_, args, ctx) => {
+    if (!ctx.user.administrator) {
+        throw new ForbiddenError("Access Denied");
+    }
+    return ctx.prisma.users();
+}
+
+export default users
