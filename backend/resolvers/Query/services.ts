f9d1b14eddbf47766796183540ba0c2558891991
Daniel Koch <daniel.koch@helsinki.fi>
2019-05-27 18:16:44 +0300

    add permission checks

--- backend/resolvers/Query/services.ts
+++ backend/resolvers/Query/services.ts
@@ -1,6 +1,10 @@
 import { Prisma } from "../../generated/prisma-client"
+import { ForbiddenError } from "apollo-server-core"
 
 const services = async (_, args, ctx) => {
+  if (!ctx.user.administrator) {
+    throw new ForbiddenError("Access Denied")
+  }
   const prisma: Prisma = ctx.prisma
   return prisma.services()
 }
