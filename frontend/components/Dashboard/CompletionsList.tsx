9c6dd3ac64673342ab02e181b8e5677347d59bf9
Heinonen Ava R <avaheino@lx8-500-109.pc.helsinki.fi>
2019-06-03 16:09:53 +0300

    I did the paginate

--- frontend/components/Dashboard/CompletionsList.tsx
+++ frontend/components/Dashboard/CompletionsList.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { ApolloClient, gql } from "apollo-boost"
 import { Query } from "react-apollo"
-import { AllCompletions as AllCompletionsData } from "../../pages/__generated__/AllCompletions"
+import { AllCompletions as AllCompletionsData } from "../__generated__/AllCompletions"
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles"
 import { Grid, Typography, CircularProgress } from "@material-ui/core"
 import CompletionCard from "./CompletionCard"
@@ -10,24 +10,32 @@ import { withRouter } from "next/router"
 import CompletionPaginator from "./CompletionPaginator"
 
 export const AllCompletionsQuery = gql`
-  query AllCompletions($course: String) {
-    completions(course: $course, first: 40) {
-      id
-      email
-      completion_language
-      created_at
-      user {
-        first_name
-        last_name
-        student_number
+  query AllCompletions($course: String, $cursor: ID) {
+    completionsPaginated(course: $course, first: 15, after: $cursor) {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
       }
-    }
-    course(slug: $course) {
-      id
-      name
+      edges {
+        node {
+          id
+          email
+          completion_language
+          created_at
+          user {
+            first_name
+            last_name
+            student_number
+          }
+        }
+        cursor
+      }
     }
   }
 `
+
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     title: {
@@ -64,26 +72,44 @@ const Completions: React.SFC<CompletionsListProps> = props => {
             </div>
           )
         }
-        console.log(data)
+        const cursor = data.completionsPaginated.pageInfo.endCursor
+
         return (
           <Grid container spacing={3} justify="center">
-            <HeaderCard course={data.course} />
-            {data.completions.map(completer => (
-              <CompletionCard completer={completer} key={completer.id} />
+            <HeaderCard course={"elements-of-ai"} />
+            {data.completionsPaginated.edges.map(completer => (
+              <CompletionCard
+                completer={completer.node}
+                key={completer.node.id}
+              />
             ))}
             <CompletionPaginator
-              onLoadMore={() =>
+              getNext={() =>
                 fetchMore({
                   query: AllCompletionsQuery,
+                  variables: { course: course, cursor: cursor },
                   updateQuery: (previousResult, { fetchMoreResult }) => {
-                    const PreviousEntry = previousResult.completions
-                    const moreCompletions = fetchMoreResult.completions
+                    const newCompletions = fetchMoreResult.completionsPaginated
+                    const newCursor = newCompletions.pageInfo.endCursor
                     return {
-                      completions: [...PreviousEntry, ...moreCompletions],
+                      cursor: newCursor,
+                      completionsPaginated: {
+                        pageInfo: {
+                          hasNextPage: newCompletions.pageInfo.hasNextPage,
+                          hasPreviousPage: true,
+                          startCursor: newCompletions.pageInfo.startCursor,
+                          endCursor: newCompletions.pageInfo.endCursor,
+                          __typename: "PageInfo",
+                        },
+                        edges: newCompletions.edges,
+                        __typename: "CompletionConnection",
+                      },
                     }
                   },
                 })
               }
+              isNext={data.completionsPaginated.pageInfo.hasNextPage}
+              hasPrevious={data.completionsPaginated.pageInfo.hasPrevious}
             />
           </Grid>
         )
