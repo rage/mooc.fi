cac9694089bfcc7552304ac0b4273294ac1d4fa2
Heinonen Ava R <avaheino@lx8-500-109.pc.helsinki.fi>
2019-06-04 14:35:04 +0300

    language filtering

--- frontend/contexes/CourseLanguageContext.ts
+++ frontend/contexes/CourseLanguageContext.ts
@@ -1,9 +1,5 @@
 import * as React from "react"
 
-const LoginStateContext = React.createContext({
-  fi: true,
-  en: true,
-  se: true,
-})
+const LoginStateContext = React.createContext("fi_FI")
 
 export default LoginStateContext
