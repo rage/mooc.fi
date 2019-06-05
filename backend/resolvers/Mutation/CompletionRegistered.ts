4caf3c66de4c0348f5720ee040034bb6952cba65
Daniel Koch <daniel.koch@helsinki.fi>
2019-06-05 10:56:55 +0300

    try number one

--- backend/resolvers/Mutation/CompletionRegistered.ts
+++ backend/resolvers/Mutation/CompletionRegistered.ts
@@ -2,40 +2,52 @@ import { Prisma, Course, User } from "../../generated/prisma-client"
 import { PrismaObjectDefinitionBlock } from "nexus-prisma/dist/blocks/objectType"
 import { arg } from "nexus/dist"
 import checkAccess from "../../accessControl"
+import { chunk } from "lodash"
 
 const registerCompletion = async (
   t: PrismaObjectDefinitionBlock<"Mutation">,
 ) => {
-  t.list.field("registerCompletion", {
-    type: "CompletionRegistered",
+  t.field("registerCompletion", {
+    type: "String",
     args: {
       completions: arg({ type: "CompletionArg", list: true }),
     },
-    resolve: (_, args, ctx) => {
+    resolve: async (_, args, ctx) => {
       checkAccess(ctx, { allowOrganizations: true, disallowAdmin: true })
       const prisma: Prisma = ctx.prisma
-      const registeredCompletions = args.completions.map(async entry => {
-        const course: Course = await prisma
-          .completion({ id: entry.completion_id })
-          .course()
-        const user: User = await prisma
-          .completion({ id: entry.completion_id })
-          .user()
-        return await prisma.createCompletionRegistered({
-          completion: { connect: { id: entry.completion_id } },
-          organization: { connect: { id: ctx.organization.id } },
-          course: { connect: { id: course.id } },
-          real_student_number: entry.student_number,
-          user: { connect: { id: user.id } },
-        })
+      let queue = chunk(args.completions, 500)
+      queue.map(async entry => {
+        await buildPromises(args.completions, ctx, prisma)
       })
-      return registeredCompletions
+      queue.map(async entry => {
+        await Promise.all(entry)
+      })
+
+      return "success"
     },
   })
 }
 
+const buildPromises = async (array, ctx, prisma) => {
+  return array.map(async entry => {
+    const course: Course = await prisma
+      .completion({ id: entry.completion_id })
+      .course()
+    const user: User = await prisma
+      .completion({ id: entry.completion_id })
+      .user()
+    return prisma.createCompletionRegistered({
+      completion: { connect: { id: entry.completion_id } },
+      organization: { connect: { id: ctx.organization.id } },
+      course: { connect: { id: course.id } },
+      real_student_number: entry.student_number,
+      user: { connect: { id: user.id } },
+    })
+  })
+}
+
 const addCompletionRegisteredMutations = (
-  t: PrismaObjectDefinitionBlock<"Mutaiton">,
+  t: PrismaObjectDefinitionBlock<"Mutation">,
 ) => {
   registerCompletion(t)
 }
