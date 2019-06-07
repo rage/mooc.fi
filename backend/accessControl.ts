bde2770ed16af2bc64fefdf01fee3e6c0af255e5
Daniel Koch <daniel.koch@helsinki.fi>
2019-06-07 12:57:37 +0300

    roles on access control

--- backend/accessControl.ts
+++ backend/accessControl.ts
@@ -16,7 +16,7 @@ const checkAccess = (
   },
 ) => {
   if (allowOrganizations && ctx.role == Role.ORGANIZATION) return true
-  if ((ctx.role == Role.ADMIN && !disallowAdmin)) return true
+  if (ctx.role == Role.ADMIN && !disallowAdmin) return true
   if (ctx.role == Role.USER && allowUsers) return true
   if (ctx.role == Role.VISITOR && allowVisitors) return true
   throw new ForbiddenError("Access Denied")
