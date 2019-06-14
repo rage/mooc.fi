6f5231c49d569ecbd3eac5b08bdd334295ffbe7d
Daniel Koch <daniel.koch@helsinki.fi>
2019-06-14 13:19:20 +0300

    updateCourse -mutation

--- backend/resolvers/Mutation/course.ts
+++ backend/resolvers/Mutation/course.ts
@@ -1,8 +1,7 @@
-import { Prisma, Course } from "../../generated/prisma-client"
+import { Prisma } from "../../generated/prisma-client"
 import { PrismaObjectDefinitionBlock } from "nexus-prisma/dist/blocks/objectType"
 import { stringArg, booleanArg, arg, idArg } from "nexus/dist"
 import checkAccess from "../../accessControl"
-
 const addCourse = async (t: PrismaObjectDefinitionBlock<"Mutation">) => {
   t.field("addCourse", {
     type: "Course",
@@ -27,24 +26,69 @@ const addCourse = async (t: PrismaObjectDefinitionBlock<"Mutation">) => {
         study_module,
       } = args
       const prisma: Prisma = ctx.prisma
-      const newCourse: Course = await prisma.createCourse({
+      return prisma.createCourse({
         name: name,
         slug: slug,
         promote: promote,
         start_point: start_point,
         photo: photo,
         course_translations: null,
-        study_module: null,
         status: status,
-        study_module: { connect: { id: study_module } },
+        study_module: (study_module != null ? { connect: { id: study_module } } : null),
       })
-      return newCourse
     },
+  })
+}
+
+const updateCourse = (t : PrismaObjectDefinitionBlock<"Mutaton">) => {
+  t.field("updateCourse", {
+    type: "Course",
+    args: {
+      id: idArg({required: false}),
+      name: stringArg(),
+      slug: stringArg(),
+      photo: stringArg(),
+      start_point: booleanArg(),
+      promote: booleanArg(),
+      status: arg({ type: "CourseStatus" }),
+      study_module: idArg(),
+    },
+    resolve: async (_, args, ctx) => {
+      checkAccess(ctx)
+      const prisma : Prisma = ctx.prisma
+      const {
+        id,
+        name,
+        slug,
+        photo,
+        start_point,
+        promote,
+        status,
+        study_module,
+      } = args
+      return prisma.updateCourse({
+        where: {
+          id: id, slug: slug
+        },
+        data: {
+          name: name,
+          slug: slug,
+          photo: photo,
+          start_point: start_point,
+          promote: promote,
+          status: status,
+          study_module: study_module
+        }
+      })
+
+    }
   })
 }
 
 const addCourseMutations = (t: PrismaObjectDefinitionBlock<"Mutation">) => {
   addCourse(t)
+  updateCourse(t)
+
 }
 
 export default addCourseMutations
