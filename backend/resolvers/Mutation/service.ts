d885e5ca04c0b468edafa55edec90fc5bdfd59bb
Daniel Koch <daniel.koch@helsinki.fi>
2019-06-14 13:55:36 +0300

    updateService -mutation

--- backend/resolvers/Mutation/service.ts
+++ backend/resolvers/Mutation/service.ts
@@ -1,6 +1,6 @@
 import { Prisma } from "../../generated/prisma-client"
 import { PrismaObjectDefinitionBlock } from "nexus-prisma/dist/blocks/objectType"
-import { stringArg } from "nexus/dist"
+import { stringArg, idArg } from "nexus/dist"
 import checkAccess from "../../accessControl"
 
 const addService = async (t: PrismaObjectDefinitionBlock<"Mutation">) => {
@@ -15,6 +15,26 @@ const addService = async (t: PrismaObjectDefinitionBlock<"Mutation">) => {
       checkAccess(ctx)
       const { url, name } = args
       return await prisma.createService({ url: url, name: name })
+    },
+  })
+}
+
+const updateService = (t: PrismaObjectDefinitionBlock<"Mutation">) => {
+  t.field("addService", {
+    type: "Service",
+    args: {
+      id: idArg({ required: true }),
+      url: stringArg(),
+      name: stringArg(),
+    },
+    resolve: (_, args, ctx) => {
+      checkAccess(ctx)
+      const { url, name, id } = args
+      const prisma: Prisma = ctx.prisma
+      return prisma.updateService({
+        where: { id: id },
+        data: { url: url, name: name },
+      })
     },
   })
 }
