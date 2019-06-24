0deb3d7644a025326f78ea9ee740b356d411f3e2
Henrik Nygren <nygrenh@gmail.com>
2019-06-24 16:43:21 +0300

    Deploy from circleci

---
+++ bin/ci-setup-google-cloud.sh
@@ -0,0 +1,15 @@
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [[ "$BRANCH" == "master" ]] || [[ "$BRANCH" == "deployment-test" ]]; then
+  echo "Configuring Google Cloud"
+  echo $GCLOUD_SERVICE_KEY >${HOME}/gcloud-service-key.json
+  gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json
+  gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
+  gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
+  echo "Configuring kubectl"
+  gcloud --quiet container clusters get-credentials moocfi-cluster
+  kubectl config set-context "$GOOGLE_CLUSTER_NAME" --namespace=moocfi
+
+  echo "Logging in to docker"
+  gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://eu.gcr.io
+fi
