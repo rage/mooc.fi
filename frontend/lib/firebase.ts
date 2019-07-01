f5eb7099fa6c3b7d9fa897f4d3f80f6848965b0a
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2019-07-01 21:39:53 +0300

    removed DRY newcoursecard (and made coursecard more complicated)

--- frontend/lib/firebase.ts
+++ frontend/lib/firebase.ts
@@ -1,9 +1,9 @@
-import * as firebase from 'firebase'
-import 'firebase/auth/dist/index.cjs'
-import 'firebase/firestore/dist/index.cjs'
-import 'firebase/database/dist/index.cjs'
+import * as firebase from "firebase"
+import "firebase/auth/dist/index.cjs"
+import "firebase/firestore/dist/index.cjs"
+import "firebase/database/dist/index.cjs"
 
-import config from '../firebase.config.js'
+import config from "../firebase.config.js"
 
 if (!firebase.apps.length) {
   firebase.initializeApp(config)
@@ -11,4 +11,4 @@ if (!firebase.apps.length) {
 
 export const auth = firebase.auth()
 export const db = firebase.database()
-export default (firebase || { storage: () => ({ ref: null }) })
+export default firebase || { storage: () => ({ ref: null }) }
