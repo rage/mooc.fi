74830cbd0008dfaa2d9c21957c8c3b23269bb490
Heinonen Ava R <avaheino@lx8-500-109.pc.helsinki.fi>
2019-07-02 11:49:23 +0300

    register completion fixes

--- frontend/i18n.js
+++ frontend/i18n.js
@@ -4,7 +4,7 @@ const I18Next = require("next-i18next/dist/commonjs")
 const NextI18Next = new I18Next({
   defaultLanguage: "fi",
   lng: "fi",
-  otherLanguages: ["en"],
+  otherLanguages: ["en", "se"],
   localeSubpaths: "foreign",
   returnObjects: true,
   serverLanguageDetection: false,
