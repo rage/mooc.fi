3c2ffab6f83bdd2ca0836d71e2ba6df330ca29b1
Daniel Koch <daniel.koch@helsinki.fi>
2019-07-03 11:51:06 +0300

    cron job (and prettied files)

--- backend/bin/parseFile.ts
+++ backend/bin/parseFile.ts
@@ -1,28 +1,42 @@
 const data = require("../appdatum.json")
 import { uniqBy } from "lodash"
-import { Prisma } from "../generated/prisma-client";
+import { Prisma } from "../generated/prisma-client"
 
 const doIt = async () => {
-    let x = data.filter(p => p.namespace === "elements-of-ai" && (p.field_name == "language" && p.value == "se"))
-    x = uniqBy(x, x.user_id)
-    console.log(x.length)
-    const prisma : Prisma = new Prisma()
-    let counter = 0
-    for(let i = 0; i < x.length; i++) {
-        const exists = await prisma.$exists.completion({user_upstream_id: x[i].user_id, course: {slug: "elements-of-ai"}})
-        if (exists) counter++
-    }
-    console.log(counter)
-    let y = data.filter(p => p.namespace === "elements-of-ai" && ((p.field_name == "language" && p.value == "en")))
-    let z = data.filter(p => p.namespace === "elements-of-ai" && (p.field_name == "country" && p.value == "Sweden")).map(p => p.user_id)
-    counter = 0
-    for(let i = 0; i < y.length; i++) {
-        if (z.includes(y[i].user_id)) counter++
-    }
-    console.log(counter)
-
+  let x = data.filter(
+    p =>
+      p.namespace === "elements-of-ai" &&
+      (p.field_name == "language" && p.value == "se"),
+  )
+  x = uniqBy(x, x.user_id)
+  console.log(x.length)
+  const prisma: Prisma = new Prisma()
+  let counter = 0
+  for (let i = 0; i < x.length; i++) {
+    const exists = await prisma.$exists.completion({
+      user_upstream_id: x[i].user_id,
+      course: { slug: "elements-of-ai" },
+    })
+    if (exists) counter++
+  }
+  console.log(counter)
+  let y = data.filter(
+    p =>
+      p.namespace === "elements-of-ai" &&
+      (p.field_name == "language" && p.value == "en"),
+  )
+  let z = data
+    .filter(
+      p =>
+        p.namespace === "elements-of-ai" &&
+        (p.field_name == "country" && p.value == "Sweden"),
+    )
+    .map(p => p.user_id)
+  counter = 0
+  for (let i = 0; i < y.length; i++) {
+    if (z.includes(y[i].user_id)) counter++
+  }
+  console.log(counter)
 }
 
 doIt().catch(e => console.log(e))
-
-
