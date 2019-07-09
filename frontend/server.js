73413cca40122085032afba48d6ee152dfeb3b2d
Daniel Koch <daniel.koch@helsinki.fi>
2019-07-09 15:48:34 +0300

    /course to /courses

--- frontend/server.js
+++ frontend/server.js
@@ -17,8 +17,8 @@ const DirectFrom = Redirects.redirects_list
   server.use(compression())
   server.use(nextI18NextMiddleware(nextI18next))
 
-  server.get("/course/:id", (req, res) => {
-    const actualPage = "/course"
+  server.get("/courses/:id", (req, res) => {
+    const actualPage = "/courses"
     const queryParams = { course: req.params.id }
     return app.render(req, res, actualPage, queryParams)
   })
