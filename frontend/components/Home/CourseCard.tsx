7fbf6509b2d1903a6a4a5f0b68f39e0558a7171b
Heinonen Ava R <avaheino@lx8-500-109.pc.helsinki.fi>
2019-07-12 16:46:14 +0300

    small css fixes

--- frontend/components/Home/CourseCard.tsx
+++ frontend/components/Home/CourseCard.tsx
@@ -13,7 +13,6 @@ const CourseImage = styled.img`
 
 const Background = styled(ButtonBase)`
   background-color: white;
-
   position: relative;
   box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
     0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2);
@@ -41,7 +40,6 @@ const TextArea = styled.div`
     height: 100%;
     width: 80%;
   }
-
   @media (min-width: 600px) and (max-width: 960px) {
     text-align: left;
     height: 100%;
