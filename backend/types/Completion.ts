bf57f0e65ecf9d8072a11ddcc28eb1841f52a77b
Daniel Koch <daniel.koch@helsinki.fi>
2019-07-24 14:09:10 +0300

    new computed field for completion

--- backend/types/Completion.ts
+++ backend/types/Completion.ts
@@ -1,5 +1,6 @@
 import { prismaObjectType } from "nexus-prisma"
 import * as resolvers from "../resolvers/Completion"
+import { Prisma, Course } from "../generated/prisma-client"
 
 const Completion = prismaObjectType({
   name: "Completion",
@@ -18,8 +19,26 @@ const Completion = prismaObjectType({
 
     t.field("user", {
       type: "User",
-      resolve: (parent, args, ctx) => resolvers.Completion(parent, args, ctx),
-    })
+      resolve: (parent, args, ctx) => resolvers.Completion(parent, args, ctx), //TODO: tämänkin voisi ehkä kirjoittaa suoraan tähän jotta tyypit saa oikein
+    }),
+      t.field("completion_link", {
+        type: "String",
+        nullable: true,
+        resolve: async (parent, args, ctx) => {
+          const prisma: Prisma = ctx.prisma
+          const course: Course = await prisma
+            .completion({ id: parent.id })
+            .course()
+          const avoinLinks = await prisma.openUniversityRegistrationLinks({
+            where: {
+              course: course,
+              language: parent.completion_language,
+            },
+          })
+          if (avoinLinks.length < 1) return null
+          return avoinLinks[0].link
+        },
+      })
   },
 })
 export default Completion
