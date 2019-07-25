2bead3ca55546dd39c8e93b8ef9bf403e9280469
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2019-07-25 15:21:22 +0300

    editor using new routes

---
+++ frontend/pages/courses/new.tsx
@@ -0,0 +1,63 @@
+import React from "react"
+import { Typography } from "@material-ui/core"
+import { NextPageContext as NextContext } from "next"
+import { isSignedIn, isAdmin } from "../../lib/authentication"
+import redirect from "../../lib/redirect"
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles"
+import AdminError from "../../components/Dashboard/AdminError"
+import { WideContainer } from "../../components/Container"
+import CourseEdit from "../../components/Dashboard/CourseEdit/CourseEdit"
+import { withRouter, SingletonRouter } from "next/router"
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    header: {
+      marginTop: "1em",
+    },
+  }),
+)
+
+interface NewCourseProps {
+  router: SingletonRouter
+  admin: boolean
+  nameSpacesRequired: string[]
+}
+
+const NewCourse = (props: NewCourseProps) => {
+  const { admin, router } = props
+  const classes = useStyles()
+
+  if (!admin) {
+    return <AdminError />
+  }
+
+  return (
+    <section>
+      <WideContainer>
+        <Typography
+          component="h1"
+          variant="h2"
+          gutterBottom={true}
+          align="center"
+          className={classes.header}
+        >
+          Create a new course
+        </Typography>
+        <CourseEdit />
+      </WideContainer>
+    </section>
+  )
+}
+
+NewCourse.getInitialProps = function(context: NextContext) {
+  const admin = isAdmin(context)
+  if (!isSignedIn(context)) {
+    redirect(context, "/sign-in")
+  }
+  return {
+    admin,
+    namespacesRequired: ["common"],
+  }
+}
+
+export default withRouter(NewCourse)
