77242e83d1e9ae588f8a495bf2bd7362482922f1
Heinonen Ava R <avaheino@lx8-500-109.pc.helsinki.fi>
2019-07-25 11:38:58 +0300

    login autocomplete fix

--- frontend/components/LanguageSwitch.tsx
+++ frontend/components/LanguageSwitch.tsx
@@ -4,11 +4,14 @@ import NextI18Next from "../i18n"
 import styled from "styled-components"
 import { useRouter } from "next/router"
 import Button from "@material-ui/core/Button"
+import Link from "next/link"
 
-const SwitchButton = styled(Button)`
+const SwitchLink = styled.a`
   font-size: 14px;
   line-height: 1.3;
   font-weight: bold;
+  display: flex;
+  flex-direction: row;
 `
 
 const LanguageSwitch = () => {
@@ -21,17 +24,20 @@ const LanguageSwitch = () => {
   }
 
   return (
-    <SwitchButton
-      onClick={() => {
-        NextI18Next.i18n.changeLanguage(
-          NextI18Next.i18n.language === "en" ? "fi" : "en",
-        )
-        router.push(path)
-      }}
-    >
-      <Language style={{ marginRight: "0.4rem" }} />
-      {NextI18Next.i18n.language === "en" ? "SUOMI" : "ENGLISH"}
-    </SwitchButton>
+    <Link href={path}>
+      <SwitchLink
+        onClick={() => {
+          NextI18Next.i18n
+            .changeLanguage(NextI18Next.i18n.language === "en" ? "fi" : "en")
+            .then(() => router.push(path))
+        }}
+      >
+        <Language style={{ marginRight: "0.4rem" }} />
+        <p style={{ marginTop: "0.2rem" }}>
+          {NextI18Next.i18n.language === "en" ? "Suomi" : "English"}
+        </p>
+      </SwitchLink>
+    </Link>
   )
 }
 
