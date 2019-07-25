cb1245647b396125671a8b2959d4a74a9a0aa417
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2019-07-25 14:36:57 +0300

    fix course card image

--- frontend/components/CourseCard.tsx
+++ frontend/components/CourseCard.tsx
@@ -22,7 +22,9 @@ const useStyles = makeStyles((theme: Theme) =>
       padding: "0.8em",
     },
     media: {
-      height: 250,
+      width: "100%",
+      height: "100%",
+      objectFit: "cover",
     },
   }),
 )
