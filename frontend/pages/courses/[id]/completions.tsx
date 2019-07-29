b6a94286115aaf51fbb9b316f15e268ea7217619
Heinonen Ava R <avaheino@lx8-500-109.pc.helsinki.fi>
2019-07-29 15:21:44 +0300

    started dashboard options to different pages

--- frontend/pages/courses/[id]/completions.tsx
+++ frontend/pages/courses/[id]/completions.tsx
@@ -4,12 +4,67 @@ import redirect from "../../../lib/redirect"
 import { NextPageContext as NextContext } from "next"
 import AdminError from "../../../components/Dashboard/AdminError"
 import CompletionsList from "../../../components/Dashboard/CompletionsList"
+import { WideContainer } from "../../../components/Container"
+import CourseLanguageContext from "../../../contexes/CourseLanguageContext"
+import LanguageSelector from "../../../components/Dashboard/LanguageSelector"
+import Typography from "@material-ui/core/Typography"
+import { withRouter, SingletonRouter } from "next/router"
+import DashboardBreadCrumbs from "../../../components/Dashboard/DashboardBreadCrumbs"
+import DashboardTabBar from "../../../components/Dashboard/DashboardTabBar"
 
-const Completions = ({ admin }: { admin: boolean }) => {
+interface CompletionsProps {
+  admin: boolean
+  router: SingletonRouter
+}
+const Completions = (props: CompletionsProps) => {
+  const { admin, router } = props
+  const [languageValue, setLanguageValue] = useState("fi_FI")
+  const handleLanguageChange = (event: React.ChangeEvent<unknown>) => {
+    setLanguageValue((event.target as HTMLInputElement).value)
+  }
+  const [selection, setSelection] = useState(0)
+
+  const handleSelectionChange = (
+    event: React.ChangeEvent<{}>,
+    value: number,
+  ) => {
+    setSelection(value)
+  }
+
+  let slug
+  if (router && router.query) {
+    slug = router.query.id
+  }
+
   if (!admin) {
     return <AdminError />
   }
-  return <CompletionsList />
+  return (
+    <CourseLanguageContext.Provider value={languageValue}>
+      <DashboardBreadCrumbs current_page={slug} />
+      <DashboardTabBar
+        value={selection}
+        handleChange={handleSelectionChange}
+        courseSlug={slug}
+      />
+      <WideContainer>
+        <Typography
+          component="h1"
+          variant="h1"
+          gutterBottom={true}
+          align="center"
+          style={{ marginTop: "2rem", marginBottom: "1rem" }}
+        >
+          Completions
+        </Typography>
+        <LanguageSelector
+          handleLanguageChange={handleLanguageChange}
+          languageValue={languageValue}
+        />
+        <CompletionsList />
+      </WideContainer>
+    </CourseLanguageContext.Provider>
+  )
 }
 
 Completions.getInitialProps = function(context: NextContext) {
@@ -24,4 +79,4 @@ Completions.getInitialProps = function(context: NextContext) {
   }
 }
 
-export default Completions
+export default withRouter(Completions)
--- frontend/pages/courses/[id]/index.tsx
+++ frontend/pages/courses/[id]/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react"
 import DashboardTabBar from "../../../components/Dashboard/DashboardTabBar"
-import CompletionsList from "../../../components/Dashboard/CompletionsList"
 import PointsList from "../../../components/Dashboard/PointsList"
 import LanguageSelector from "../../../components/Dashboard/LanguageSelector"
 import DashboardBreadCrumbs from "../../../components/Dashboard/DashboardBreadCrumbs"
@@ -12,16 +11,6 @@ import { NextPageContext as NextContext } from "next"
 import { WideContainer } from "../../../components/Container"
 import { withRouter, SingletonRouter } from "next/router"
 import CourseLanguageContext from "../../../contexes/CourseLanguageContext"
-
-//map selection value of tab navigation
-//to the component to be rendered
-const MapTypeToComponent = new Map(
-  Object.entries({
-    1: <CompletionsList />,
-    2: <PointsList />,
-    0: <CourseDashboard />,
-  }),
-)
 
 interface CourseProps {
   admin: boolean
@@ -64,13 +53,14 @@ const Course = (props: CourseProps) => {
         <DashboardTabBar
           value={selection}
           handleChange={handleSelectionChange}
+          courseSlug={slug}
         />
         <WideContainer>
           <LanguageSelector
             handleLanguageChange={handleLanguageChange}
             languageValue={languageValue}
           />
-          {MapTypeToComponent.get(selection.toString())}
+          <CourseDashboard />
         </WideContainer>
       </section>
     </CourseLanguageContext.Provider>
