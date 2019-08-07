a37d175ff62da171a94021a41c2d6b56fe5b8ddb
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2019-08-07 10:30:45 +0300

    require images

--- frontend/components/Home/ModuleNaviCard.tsx
+++ frontend/components/Home/ModuleNaviCard.tsx
@@ -87,8 +87,8 @@ const NaviCardBodyText = styled(Typography)`
 `
 function ModuleNaviCard({ module }: { module: ObjectifiedModule }) {
   const imageUrl = module.image
-    ? `../../static/images/${module.image}`
-    : `../../static/images/${module.slug}.jpg`
+    ? require(`../../static/images/${module.image}`)
+    : require(`../../static/images/${module.slug}.jpg`)
 
   return (
     <Grid item xs={12} md={6} lg={6}>
