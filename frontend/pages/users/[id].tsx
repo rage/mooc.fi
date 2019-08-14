7c346b5577b1fd15e25b9b508e900bf1348ff55c
Daniel Koch <daniel.koch@helsinki.fi>
2019-08-14 12:19:24 +0300

    adminpane + user course settingses query + modified it from backend

--- frontend/pages/users/[id].tsx
+++ frontend/pages/users/[id].tsx
@@ -1,8 +1,55 @@
 import * as React from "react"
-import Container from "/components/Container"
+import Container from "../../components/Container"
+import { NextPageContext } from "next"
+import { isAdmin, isSignedIn } from "../../lib/authentication"
+import redirect from "../../lib/redirect"
+import AdminError from "../../components/Dashboard/AdminError"
+import gql from "graphql-tag"
 
-const UserPage = () => {
+interface UserPageProps {
+  namespacesRequired: string[]
+  t: Function
+  i18n: any
+  admin: boolean
+}
+
+const UserPage = (props: UserPageProps) => {
+  if (!props.admin) {
+    return <AdminError />
+  }
+
   return <Container>User Page</Container>
 }
 
+UserPage.getInitialProps = function(context: NextPageContext) {
+  const admin = isAdmin(context)
+  if (!isSignedIn(context)) {
+    redirect(context, "/sign-in")
+  }
+  return {
+    admin,
+    namespacesRequired: ["common"],
+  }
+}
 export default UserPage
+
+const GET_DATA = gql`
+  query UserCourseSettingses($upstream_id: number) {
+    UserCourseSettingses(user_upstream_id: $upstream_id, first: 50) {
+      edges {
+        node {
+          id
+          course {
+            name
+          }
+          language
+          country
+          research
+          marketing
+          course_variant
+          other
+        }
+      }
+    }
+  }
+`
