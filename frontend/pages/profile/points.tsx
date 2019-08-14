4e0cae2e85217a1296114d73a7028a4fa2757c5a
Heinonen Ava R <avaheino@lx8-500-109.pc.helsinki.fi>
2019-08-14 14:14:01 +0300

    sorted merge with master

--- frontend/pages/profile/points.tsx
+++ frontend/pages/profile/points.tsx
@@ -3,7 +3,7 @@ import { isSignedIn } from "../../lib/authentication"
 import { NextPageContext as NextContext } from "next"
 import redirect from "../../lib/redirect"
 import { gql } from "apollo-boost"
-import { useQuery } from "react-apollo-hooks"
+import { useQuery } from "@apollo/react-hooks"
 
 export const UserPointsQuery = gql`
   query UserPoints {
