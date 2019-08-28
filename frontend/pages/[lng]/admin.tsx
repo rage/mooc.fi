ea92afd478b0edd8b51909083ddae90155cd7acb
Heinonen Ava R <avaheino@lx8-500-109.pc.helsinki.fi>
2019-08-28 12:24:26 +0300

    breadcrumb links fixed

--- frontend/pages/[lng]/admin.tsx
+++ frontend/pages/[lng]/admin.tsx
@@ -8,6 +8,7 @@ import redirect from "/lib/redirect"
 import AdminError from "/components/Dashboard/AdminError"
 import LanguageContext from "/contexes/LanguageContext"
 import { useContext } from "react"
+import DashboardBreadCrumbs from "/components/Dashboard/DashboardBreadCrumbs"
 
 const StyledCard = styled(Card)`
   width: 100%;
@@ -21,29 +22,32 @@ const Admin = (admin: boolean) => {
   }
   const { language } = useContext(LanguageContext)
   return (
-    <Container>
-      <Link href={`/${language}/courses`}>
-        <StyledCard>
-          <Typography variant="h2" component="h2">
-            Courses
-          </Typography>
-        </StyledCard>
-      </Link>
-      <Link href={`/${language}/study-modules`}>
-        <StyledCard>
-          <Typography variant="h2" component="h2">
-            Study Modules
-          </Typography>
-        </StyledCard>
-      </Link>
-      <Link href={`/${language}/users/search`}>
-        <StyledCard>
-          <Typography variant="h2" component="h2">
-            User search
-          </Typography>
-        </StyledCard>
-      </Link>
-    </Container>
+    <>
+      <DashboardBreadCrumbs />
+      <Container>
+        <Link href={`/${language}/courses`}>
+          <StyledCard>
+            <Typography variant="h2" component="h2">
+              Courses
+            </Typography>
+          </StyledCard>
+        </Link>
+        <Link href={`/${language}/study-modules`}>
+          <StyledCard>
+            <Typography variant="h2" component="h2">
+              Study Modules
+            </Typography>
+          </StyledCard>
+        </Link>
+        <Link href={`/${language}/users/search`}>
+          <StyledCard>
+            <Typography variant="h2" component="h2">
+              User search
+            </Typography>
+          </StyledCard>
+        </Link>
+      </Container>
+    </>
   )
 }
 
