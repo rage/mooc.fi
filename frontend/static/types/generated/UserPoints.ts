27726c9e11ca95a963f7a30db8cda505a5208c09
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2019-08-29 16:13:21 +0300

    userpoints fragment name fix

--- frontend/static/types/generated/UserPoints.ts
+++ frontend/static/types/generated/UserPoints.ts
@@ -3,50 +3,54 @@
 // This file was automatically generated and should not be edited.
 
 // ====================================================
-// GraphQL fragment: UserPoints
+// GraphQL query operation: UserPoints
 // ====================================================
 
-export interface UserPoints_user_course_progresses_course {
+export interface UserPoints_currentUser_user_course_progresses_course {
   __typename: "Course"
   id: any
   name: string
 }
 
-export interface UserPoints_user_course_progresses_user_course_service_progresses_course {
+export interface UserPoints_currentUser_user_course_progresses_user_course_service_progresses_course {
   __typename: "Course"
   id: any
   name: string
 }
 
-export interface UserPoints_user_course_progresses_user_course_service_progresses_service {
+export interface UserPoints_currentUser_user_course_progresses_user_course_service_progresses_service {
   __typename: "Service"
   id: any
   name: string
 }
 
-export interface UserPoints_user_course_progresses_user_course_service_progresses {
+export interface UserPoints_currentUser_user_course_progresses_user_course_service_progresses {
   __typename: "UserCourseServiceProgress"
-  course: UserPoints_user_course_progresses_user_course_service_progresses_course
-  service: UserPoints_user_course_progresses_user_course_service_progresses_service
+  course: UserPoints_currentUser_user_course_progresses_user_course_service_progresses_course
+  service: UserPoints_currentUser_user_course_progresses_user_course_service_progresses_service
   progress: any
 }
 
-export interface UserPoints_user_course_progresses {
+export interface UserPoints_currentUser_user_course_progresses {
   __typename: "UserCourseProgress"
   id: any
-  course: UserPoints_user_course_progresses_course
+  course: UserPoints_currentUser_user_course_progresses_course
   progress: any
   user_course_service_progresses:
-    | UserPoints_user_course_progresses_user_course_service_progresses[]
+    | UserPoints_currentUser_user_course_progresses_user_course_service_progresses[]
     | null
 }
 
-export interface UserPoints {
+export interface UserPoints_currentUser {
   __typename: "User"
   id: any
   first_name: string | null
   last_name: string | null
   email: string
   student_number: string | null
-  user_course_progresses: UserPoints_user_course_progresses[] | null
+  user_course_progresses: UserPoints_currentUser_user_course_progresses[] | null
+}
+
+export interface UserPoints {
+  currentUser: UserPoints_currentUser | null
 }
