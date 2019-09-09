a4459249d8bf1e9e618efc53cadb6749d980077c
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2019-09-09 12:55:36 +0300

    no completions in different languages

--- frontend/pages/[lng]/register-completion/[slug].tsx
+++ frontend/pages/[lng]/register-completion/[slug].tsx
@@ -86,10 +86,7 @@ const RegisterCompletion = (props: RegisterCompletionPageProps) => {
   const { language } = useContext(LanguageContext)
 
   const t = getRegisterCompletionTranslator(language)
-  const { loading, error, data } = useQuery<UserOverViewData>(
-    UserOverViewQuery,
-    { variables: { language } },
-  )
+  const { loading, error, data } = useQuery<UserOverViewData>(UserOverViewQuery)
 
   if (error) {
     return (
