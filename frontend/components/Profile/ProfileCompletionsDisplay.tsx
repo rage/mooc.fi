d69fadedecad3f5b8e6b362acf28a6fe8ff7a0ef
Heinonen Ava R <avaheino@lx8-500-109.pc.helsinki.fi>
2019-10-09 15:47:59 +0300

    linking

--- frontend/components/Profile/ProfileCompletionsDisplay.tsx
+++ frontend/components/Profile/ProfileCompletionsDisplay.tsx
@@ -1,16 +1,32 @@
-import React from "react"
+import React, { useContext } from "react"
 import CompletionListItem from "/components/CompletionListItem"
-
+import LanguageContext from "/contexes/LanguageContext"
+import getProfileTranslator from "/translations/profile"
+import LangLink from "/components/LangLink"
+import Button from "@material-ui/core/Button"
 interface CompletionsProps {
   completions: any[]
 }
 const ProfileCompletionsDisplay = (props: CompletionsProps) => {
   const { completions } = props
+  const lng = useContext(LanguageContext)
+  const t = getProfileTranslator(lng.language)
+  console.log(t)
   return (
     <>
-      {completions.map(c => (
+      {completions.slice(0, 10).map(c => (
         <CompletionListItem listItem={c} key={c.id} />
       ))}
+      <LangLink
+        href="/[lng]/profile/completions"
+        as={`/${lng.language}/profile/completions`}
+      >
+        <Button variant="text" fullWidth>
+          <a href={`/${lng.language}/profile/completions`}>
+            {t("seeCompletions")}
+          </a>
+        </Button>
+      </LangLink>
     </>
   )
 }
