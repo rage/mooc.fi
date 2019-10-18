c28abb358a3a53ab225ffd4413d9da76ed5254a6
Daniel Koch <daniel.koch@helsinki.fi>
2019-10-18 13:03:20 +0300

    mutations for new fields

--- backend/resolvers/Mutation/userCourseProgress.ts
+++ backend/resolvers/Mutation/userCourseProgress.ts
@@ -1,6 +1,6 @@
 import { Prisma } from "../../generated/prisma-client"
 import { PrismaObjectDefinitionBlock } from "nexus-prisma/dist/blocks/objectType"
-import { idArg, arg } from "nexus/dist"
+import { idArg, arg, floatArg } from "nexus/dist"
 import checkAccess from "../../accessControl"
 
 const addUserCourseProgress = (t: PrismaObjectDefinitionBlock<"Mutation">) => {
@@ -10,15 +10,19 @@ const addUserCourseProgress = (t: PrismaObjectDefinitionBlock<"Mutation">) => {
       user_id: idArg({ required: true }),
       course_id: idArg({ required: true }),
       progress: arg({ type: "PointsByGroup", required: true }),
+      max_points: floatArg(),
+      n_points: floatArg(),
     },
     resolve: (_, args, ctx) => {
       checkAccess(ctx)
-      const { user_id, course_id, progress } = args
+      const { user_id, course_id, progress, max_points, n_points } = args
       const prisma: Prisma = ctx.prisma
       return prisma.createUserCourseProgress({
         user: { connect: { id: user_id } },
         course: { connect: { id: course_id } },
         progress: progress,
+        max_points,
+        n_points,
       })
     },
   })
