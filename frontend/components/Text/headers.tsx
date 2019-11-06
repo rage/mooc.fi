de74f76ba06c37dd34fee85e99cd8e172d1f2ce2
Ava Heinonen <a.v.a.r.heinonen@gmail.com>
2019-11-06 17:00:06 +0200

    title and start subtitle with background

--- frontend/components/Text/headers.tsx
+++ frontend/components/Text/headers.tsx
@@ -9,7 +9,7 @@ export const HOneNoBackground = styled(Typography)`
 
   @media (min-width: 600px) {
     padding-top: 1em;
-    padding-bottom: 1em;
+    padding-bottom: 0.7em;
   }
 
   @media (min-width: 960px) {
@@ -17,3 +17,24 @@ export const HOneNoBackground = styled(Typography)`
     padding-right: 1em;
   }
 `
+export const SubtitleNoBackground = styled(Typography)`
+  padding-bottom: 1em;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  font-size: 2em;
+`
+
+export const HOneBackground = styled(Typography)`
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 0.7em;
+  padding-bottom: 0.7em;
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
+  margin-top: 1em;
+  margin-bottom: 0.7em;
+  background-color: white;
+  width: 45%;
+`
+
+export const HTwoBackground = styled(Typography)``
