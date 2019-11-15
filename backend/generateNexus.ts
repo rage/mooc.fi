0c43f5f7db1bde8d2c5f70bea34722db974aa3ce
Henrik Nygren <nygrenh@gmail.com>
2019-11-15 16:01:09 +0200

    Generate nexus on ci

---
+++ backend/generateNexus.ts
@@ -0,0 +1,31 @@
+import { prisma } from "./generated/prisma-client"
+import datamodelInfo from "./generated/nexus-prisma"
+import * as path from "path"
+import { makePrismaSchema } from "nexus-prisma"
+import * as types from "./types"
+
+const schema = makePrismaSchema({
+  types: [types],
+
+  prisma: {
+    datamodelInfo,
+    client: prisma,
+  },
+
+  outputs: {
+    schema: path.join(__dirname, "./generated/schema.graphql"),
+    typegen: path.join(__dirname, "./generated/nexus.ts"),
+  },
+
+  typegenAutoConfig: {
+    sources: [
+      {
+        source: path.join(__dirname, "./context.ts"),
+        alias: "ctx",
+      },
+    ],
+    contextType: "ctx.Context",
+  },
+})
+
+console.log(schema)
