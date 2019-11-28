146dee552de6ff32458704152446305f12c272fd
Ava Heinonen <a.v.a.r.heinonen@gmail.com>
2019-11-28 15:35:21 +0200

    New error message from grphql errors

--- frontend/pages/[lng]/users/[id]/completions.tsx
+++ frontend/pages/[lng]/users/[id]/completions.tsx
@@ -9,6 +9,8 @@ import Container from "/components/Container"
 import Completions from "/components/Completions"
 import AdminError from "/components/Dashboard/AdminError"
 import { useQueryParameter } from "/util/useQueryParameter"
+import Spinner from "/components/Spinner"
+import ModifiableErrorMessage from "/components/ModifiableErrorMessage"
 
 export const UserOverViewQuery = gql`
   query ShowUserUserOverView($upstream_id: Int) {
@@ -43,14 +45,14 @@ function CompletionsPage(props: CompletionsProps) {
 
   if (error) {
     return (
-      <div>
-        Error: <pre>{JSON.stringify(error, undefined, 2)}</pre>
-      </div>
+      <ModifiableErrorMessage
+        ErrorMessage={JSON.stringify(error, undefined, 2)}
+      />
     )
   }
 
   if (loading || !data) {
-    return <div>Loading</div>
+    return <Spinner />
   }
 
   return (
