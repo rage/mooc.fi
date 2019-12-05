a1d33833476850e69aeb8f8ef5fef84846c42cec
Ava Heinonen <a.v.a.r.heinonen@gmail.com>
2019-12-05 16:54:51 +0200

    styled

--- frontend/components/Installation/NoOsMessage.tsx
+++ frontend/components/Installation/NoOsMessage.tsx
@@ -3,20 +3,20 @@ import Card from "@material-ui/core/Card"
 import styled from "styled-components"
 import { H1NoBackground, SubtitleNoBackground } from "/components/Text/headers"
 
-const Background = styled(Card)`
+const RedBackground = styled(Card)`
   background-color: #822900;
   color: white;
 `
 const NoOsMessage = () => {
   return (
-    <Background>
+    <RedBackground>
       <H1NoBackground component="h2" variant="h4" align="center">
         Could not detect Operating system
       </H1NoBackground>
       <SubtitleNoBackground align="center">
         Please select your operating system from the menu above
       </SubtitleNoBackground>
-    </Background>
+    </RedBackground>
   )
 }
 
