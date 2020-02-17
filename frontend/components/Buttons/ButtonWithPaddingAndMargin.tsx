010801a2c24a1f01f1160a805279b133a2f0e086
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2020-02-17 16:16:31 +0200

    button default text color check swapped

--- frontend/components/Buttons/ButtonWithPaddingAndMargin.tsx
+++ frontend/components/Buttons/ButtonWithPaddingAndMargin.tsx
@@ -1,9 +1,9 @@
 import styled from "styled-components"
 import Button from "@material-ui/core/Button"
 
-export const ButtonWithPaddingAndMargin = styled(Button)<{ color: string }>`
+export const ButtonWithPaddingAndMargin = styled(Button)<{ color?: string }>`
   margin: 0.5rem;
-  color: ${({ color }) => (color === "primary" ? "#FFFFFF" : "#4e4637")};
+  color: ${({ color }) => (color === "secondary" ? "#4e4637" : "#FFFFFF")};
   font-size: 18px;
   padding: 0.5em;
 `
