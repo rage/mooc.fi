bb7403b6bf1f9395ed3f430e73c690c60e508152
Ava Heinonen <a.v.a.r.heinonen@gmail.com>
2020-02-20 12:28:26 +0200

    small ui fixes to forms

--- frontend/components/Dashboard/Editor/StudyModule/StudyModuleEditForm.tsx
+++ frontend/components/Dashboard/Editor/StudyModule/StudyModuleEditForm.tsx
@@ -36,6 +36,12 @@ import { LanguageEntry } from "/components/Surfaces/LanguageEntryGrid"
 import getModulesTranslator from "/translations/study-modules"
 import LanguageContext from "/contexes/LanguageContext"
 import { useConfirm } from "material-ui-confirm"
+import { FormSubtitle } from "/components/Dashboard/Editor/Course/CourseEditForm"
+
+const FormContainer = styled.div`
+  background-color: white;
+  padding: 1rem;
+`
 
 const ModuleImage = styled.img<{ error?: boolean }>`
   object-fit: cover;
@@ -78,184 +84,194 @@ const RenderForm = ({
   }, [image, slug])
 
   return (
-    <Form>
-      <Grid container direction="row" spacing={2}>
-        <Grid item xs={10}>
-          <StyledFieldWithAnchor
-            name="name"
-            type="text"
-            label={t("moduleName")}
-            error={errors.name}
-            fullWidth
-            autoComplete="off"
-            variant="outlined"
-            component={StyledTextField}
-          />
+    <FormContainer>
+      <Form>
+        <FormSubtitle variant="h6" component="h3" align="center">
+          {t("moduleDetailsTitle")}
+        </FormSubtitle>
+        <Grid container direction="row" spacing={2}>
+          <Grid item xs={10}>
+            <StyledFieldWithAnchor
+              name="name"
+              type="text"
+              label={t("moduleName")}
+              error={errors.name}
+              fullWidth
+              autoComplete="off"
+              variant="outlined"
+              component={StyledTextField}
+            />
+          </Grid>
+          <Grid item xs={2}>
+            <StyledFieldWithAnchor
+              name="order"
+              type="number"
+              label={t("moduleOrder")}
+              error={errors.order}
+              fullWidth
+              autoComplete="off"
+              variant="outlined"
+              component={StyledTextField}
+            />
+          </Grid>
         </Grid>
-        <Grid item xs={2}>
-          <StyledFieldWithAnchor
-            name="order"
-            type="number"
-            label={t("moduleOrder")}
-            error={errors.order}
-            fullWidth
-            autoComplete="off"
-            variant="outlined"
-            component={StyledTextField}
-          />
-        </Grid>
-      </Grid>
-      <StyledFieldWithAnchor
-        name="new_slug"
-        type="text"
-        label={t("moduleSlug")}
-        error={errors.new_slug}
-        fullWidth
-        variant="outlined"
-        autoComplete="off"
-        component={StyledTextField}
-      />
-      <StyledFieldWithAnchor
-        name="image"
-        type="text"
-        label={t("moduleImageName")}
-        fullWidth
-        variant="outlined"
-        autoComplete="off"
-        component={StyledTextField}
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">
-              <Tooltip title={t("moduleImageTooltip")}>
-                <HelpIcon />
-              </Tooltip>
-            </InputAdornment>
-          ),
-        }}
-      />
-      <OutlinedFormControl variant="outlined" error={!!imageError}>
-        <OutlinedInputLabel shrink>
-          {t("moduleImagePreview")}
-        </OutlinedInputLabel>
-        <OutlinedFormGroup>
-          <ModuleImage
-            src={imageFilename}
-            error={!!imageError}
-            onError={() => setImageError(t("moduleImageError"))}
-            onLoad={() => setImageError("")}
-          />
-          {!!imageError ? (
-            <Typography variant="body2" style={{ color: "#FF0000" }}>
-              {imageError}
-            </Typography>
-          ) : null}
-        </OutlinedFormGroup>
-      </OutlinedFormControl>
-
-      <Grid container direction="column">
-        <FieldArray
-          name="study_module_translations"
-          render={helpers => (
-            <>
-              {values?.study_module_translations?.map(
-                (_: any, index: number) => (
-                  <LanguageEntry item key={`translation-${index}`}>
-                    <EntryContainer elevation={2}>
-                      <StyledFieldWithAnchor
-                        name={`study_module_translations[${index}].language`}
-                        type="select"
-                        label={t("moduleLanguage")}
-                        errors={[
-                          getIn(
-                            errors,
-                            `study_module_translations[${index}].language`,
-                          ),
-                        ]}
-                        fullWidth
-                        variant="outlined"
-                        select
-                        autoComplete="off"
-                        component={StyledTextField}
-                      >
-                        {languages(t).map(option => (
-                          <MenuItem key={option.value} value={option.value}>
-                            {option.label}
-                          </MenuItem>
-                        ))}
-                      </StyledFieldWithAnchor>
-                      <StyledFieldWithAnchor
-                        name={`study_module_translations[${index}].name`}
-                        type="text"
-                        label={t("moduleName")}
-                        error={getIn(
-                          errors,
-                          `study_module_translations[${index}].name`,
-                        )}
-                        fullWidth
-                        autoComplete="off"
-                        variant="outlined"
-                        component={StyledTextField}
-                      />
-                      <StyledFieldWithAnchor
-                        name={`study_module_translations[${index}].description`}
-                        type="textarea"
-                        label={t("moduleDescription")}
-                        error={getIn(
-                          errors,
-                          `study_module_translations[${index}].description`,
-                        )}
-                        fullWidth
-                        multiline
-                        rows={5}
-                        autoComplete="off"
-                        variant="outlined"
-                        component={StyledTextField}
-                      />
-                      <br />
-                      <Grid container justify="flex-end">
-                        <StyledButton
-                          variant="contained"
-                          disabled={isSubmitting}
-                          color="secondary"
-                          onClick={() =>
-                            confirm({
-                              title: t("moduleConfirmationTitle"),
-                              description: t("moduleConfirmationContent"),
-                              confirmationText: t("moduleConfirmationYes"),
-                              cancellationText: t("moduleConfirmationNo"),
-                            }).then(() => helpers.remove(index))
-                          }
-                        >
-                          {t("moduleRemoveTranslation")}
-                        </StyledButton>
-                      </Grid>
-                    </EntryContainer>
-                  </LanguageEntry>
-                ),
-              ) ?? (
-                <EntryContainer elevation={2}>
-                  <Typography variant="body1">
-                    {t("moduleAtLeastOneTranslation")}
-                  </Typography>
-                </EntryContainer>
-              )}
-              {values?.study_module_translations?.length <
-                languages(t).length && (
-                <FormSubmitButton
-                  variant="contained"
-                  color="primary"
-                  fullWidth
-                  disabled={isSubmitting}
-                  onClick={() => helpers.push({ ...initialTranslation })}
-                >
-                  {t("moduleAddTranslation")}
-                </FormSubmitButton>
-              )}
-            </>
-          )}
+        <StyledFieldWithAnchor
+          name="new_slug"
+          type="text"
+          label={t("moduleSlug")}
+          error={errors.new_slug}
+          fullWidth
+          variant="outlined"
+          autoComplete="off"
+          component={StyledTextField}
         />
-      </Grid>
-    </Form>
+        <StyledFieldWithAnchor
+          name="image"
+          type="text"
+          label={t("moduleImageName")}
+          fullWidth
+          variant="outlined"
+          autoComplete="off"
+          component={StyledTextField}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <Tooltip title={t("moduleImageTooltip")}>
+                  <HelpIcon />
+                </Tooltip>
+              </InputAdornment>
+            ),
+          }}
+        />
+        <FormSubtitle variant="h6" component="h3" align="center">
+          {t("modulePhotoTitle")}
+        </FormSubtitle>
+        <OutlinedFormControl variant="outlined" error={!!imageError}>
+          <OutlinedInputLabel shrink>
+            {t("moduleImagePreview")}
+          </OutlinedInputLabel>
+          <OutlinedFormGroup>
+            <ModuleImage
+              src={imageFilename}
+              error={!!imageError}
+              onError={() => setImageError(t("moduleImageError"))}
+              onLoad={() => setImageError("")}
+            />
+            {!!imageError ? (
+              <Typography variant="body2" style={{ color: "#FF0000" }}>
+                {imageError}
+              </Typography>
+            ) : null}
+          </OutlinedFormGroup>
+        </OutlinedFormControl>
+        <FormSubtitle variant="h6" component="h3" align="center">
+          {t("moduleTranslationsTitle")}
+        </FormSubtitle>
+        <Grid container direction="column">
+          <FieldArray
+            name="study_module_translations"
+            render={helpers => (
+              <>
+                {values?.study_module_translations?.map(
+                  (_: any, index: number) => (
+                    <LanguageEntry item key={`translation-${index}`}>
+                      <EntryContainer elevation={2}>
+                        <StyledFieldWithAnchor
+                          name={`study_module_translations[${index}].language`}
+                          type="select"
+                          label={t("moduleLanguage")}
+                          errors={[
+                            getIn(
+                              errors,
+                              `study_module_translations[${index}].language`,
+                            ),
+                          ]}
+                          fullWidth
+                          variant="outlined"
+                          select
+                          autoComplete="off"
+                          component={StyledTextField}
+                        >
+                          {languages(t).map(option => (
+                            <MenuItem key={option.value} value={option.value}>
+                              {option.label}
+                            </MenuItem>
+                          ))}
+                        </StyledFieldWithAnchor>
+                        <StyledFieldWithAnchor
+                          name={`study_module_translations[${index}].name`}
+                          type="text"
+                          label={t("moduleName")}
+                          error={getIn(
+                            errors,
+                            `study_module_translations[${index}].name`,
+                          )}
+                          fullWidth
+                          autoComplete="off"
+                          variant="outlined"
+                          component={StyledTextField}
+                        />
+                        <StyledFieldWithAnchor
+                          name={`study_module_translations[${index}].description`}
+                          type="textarea"
+                          label={t("moduleDescription")}
+                          error={getIn(
+                            errors,
+                            `study_module_translations[${index}].description`,
+                          )}
+                          fullWidth
+                          multiline
+                          rows={5}
+                          autoComplete="off"
+                          variant="outlined"
+                          component={StyledTextField}
+                        />
+                        <br />
+                        <Grid container justify="flex-end">
+                          <StyledButton
+                            variant="contained"
+                            disabled={isSubmitting}
+                            color="secondary"
+                            onClick={() =>
+                              confirm({
+                                title: t("moduleConfirmationTitle"),
+                                description: t("moduleConfirmationContent"),
+                                confirmationText: t("moduleConfirmationYes"),
+                                cancellationText: t("moduleConfirmationNo"),
+                              }).then(() => helpers.remove(index))
+                            }
+                          >
+                            {t("moduleRemoveTranslation")}
+                          </StyledButton>
+                        </Grid>
+                      </EntryContainer>
+                    </LanguageEntry>
+                  ),
+                ) ?? (
+                  <EntryContainer elevation={2}>
+                    <Typography variant="body1">
+                      {t("moduleAtLeastOneTranslation")}
+                    </Typography>
+                  </EntryContainer>
+                )}
+                {values?.study_module_translations?.length <
+                  languages(t).length && (
+                  <FormSubmitButton
+                    variant="contained"
+                    color="primary"
+                    fullWidth
+                    disabled={isSubmitting}
+                    onClick={() => helpers.push({ ...initialTranslation })}
+                  >
+                    {t("moduleAddTranslation")}
+                  </FormSubmitButton>
+                )}
+              </>
+            )}
+          />
+        </Grid>
+      </Form>
+    </FormContainer>
   )
 }
 
