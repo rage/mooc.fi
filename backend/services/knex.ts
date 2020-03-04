732b67898e55ecb49145ed987fc1e91931918c9c
Perkoila Kalle M <perkoila@lx8-500-038.pc.helsinki.fi>
2020-03-04 15:42:44 +0200

    exercise completions towards course completion

---
+++ backend/services/knex.ts
@@ -0,0 +1,16 @@
+import * as knex from "knex"
+
+export default knex({
+  client: "pg",
+  connection: {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  searchPath:
+    process.env.NODE_ENV === "production"
+      ? ["moocfi$production"]
+      : ["default$default"],
+})
