26b8a72a28586fbb12155e351154b567e9c9cf49
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2020-03-11 13:18:49 +0200

    implemented alerts below the header; signup now shows registration email thing there

---
+++ frontend/pages/[lng]/confirm-email.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { RegularContainer } from "/components/Container"
+import ConfirmEmail from "/components/ConfirmEmail"
+import withSignedIn from "/lib/with-signed-in"
+
+const ConfirmEmailPage = () => {
+  return (
+    <div>
+      <RegularContainer>
+        <ConfirmEmail onComplete={() => {}} />
+      </RegularContainer>
+    </div>
+  )
+}
+
+export default withSignedIn(ConfirmEmailPage)
