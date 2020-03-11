b7763c12fe71e7aaf00e9d9afe3176d470ca4d8b
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2020-03-11 13:18:49 +0200

    implemented alerts below the header; signup now shows registration email thing there

---
+++ frontend/components/HeaderBar/Alerts.tsx
@@ -0,0 +1,29 @@
+import React, { useContext } from "react"
+import AlertContext from "/contexes/AlertContext"
+import Alert from "@material-ui/lab/Alert"
+import AlertTitle from "@material-ui/lab/AlertTitle"
+import { useRouter } from "next/router"
+
+const Alerts = () => {
+  const { alerts, removeAlert } = useContext(AlertContext)
+  const router = useRouter()
+
+  return (
+    <>
+      {alerts
+        .filter(alert => !alert.ignorePages?.includes(router.pathname))
+        .map((alert, idx) => (
+          <Alert
+            key={`alert-${idx}`}
+            onClose={() => removeAlert(alert)}
+            severity={alert.severity || "info"}
+          >
+            {alert.title ? <AlertTitle>{alert.title}</AlertTitle> : null}
+            {alert.message || alert.component}
+          </Alert>
+        ))}
+    </>
+  )
+}
+
+export default Alerts
--- frontend/contexes/LoginStateContext.ts
+++ frontend/contexes/LoginStateContext.ts
@@ -1,10 +1,12 @@
 import * as React from "react"
 import { UserOverView_currentUser } from "/static/types/generated/UserOverView"
-interface LoginState {
+
+export interface LoginState {
   loggedIn: boolean
   logInOrOut: () => void
   currentUser?: UserOverView_currentUser
 }
+
 const LoginStateContext = React.createContext<LoginState>({
   loggedIn: false,
   logInOrOut: () => {},
