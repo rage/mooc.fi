f0dff81565ff6a084b5e2c6f0723fa5d40773f00
Henrik Nygren <nygrenh@gmail.com>
2020-03-26 13:55:20 +0200

    Automatically update cronjob on deployment

--- bin/ci-update-deployments.sh
+++ bin/ci-update-deployments.sh
@@ -28,6 +28,7 @@ envsubst < "$CURRENT_DIR/../kubernetes/frontend-deployment.yml" > "$CURRENT_DIR/
 envsubst < "$CURRENT_DIR/../kubernetes/fetch-user-app-datum-cronjob.yml" > "$CURRENT_DIR/../updated-kubernetes-configs/fetch-user-app-datum-cronjob.yml"
 envsubst < "$CURRENT_DIR/../kubernetes/send-ai-statistics-cronjob.yml" > "$CURRENT_DIR/../updated-kubernetes-configs/send-ai-statistics-cronjob.yml"
 envsubst < "$CURRENT_DIR/../kubernetes/fetch-avoin-links-cronjob.yml" > "$CURRENT_DIR/../updated-kubernetes-configs/fetch-avoin-links-cronjob.yml"
+envsubst < "$CURRENT_DIR/../kubernetes/update-course-statuses-cronjob.yml" > "$CURRENT_DIR/../updated-kubernetes-configs/update-course-statuses-cronjob.yml"
 envsubst < "$CURRENT_DIR/../kubernetes/import-organizations-cronjob.yml" > "$CURRENT_DIR/../updated-kubernetes-configs/import-organizations-cronjob.yml"
 envsubst < "$CURRENT_DIR/../kubernetes/kafka/kafka-consumer-user-course-progress.yml" > "$CURRENT_DIR/../updated-kubernetes-configs/kafka-consumer-user-course-progress.yml"
 envsubst < "$CURRENT_DIR/../kubernetes/kafka/kafka-consumer-exercises.yml" > "$CURRENT_DIR/../updated-kubernetes-configs/kafka-consumer-exercises.yml"
