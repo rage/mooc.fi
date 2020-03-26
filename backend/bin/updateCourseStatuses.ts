99fa5809cd89a5723edcb32ffac5d53ae7f616fc
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2020-03-26 13:27:12 +0200

    script to update course status

---
+++ backend/bin/updateCourseStatuses.ts
@@ -0,0 +1,61 @@
+import { Prisma, Course } from "../generated/prisma-client"
+import KafkaProducer, { ProducerMessage } from "../services/kafkaProducer"
+import { DateTime } from "luxon"
+
+const prisma = new Prisma()
+
+const updateCourseStatuses = async () => {
+  const courses = await prisma.courses()
+  const kafkaProducer = new KafkaProducer()
+
+  Promise.all(
+    courses.map(async (course: Course) => {
+      const { status } = course
+
+      let newStatus = status
+
+      const courseStartDate = course.start_date
+        ? DateTime.fromISO(course.start_date)
+        : null
+      const courseEndDate = course.end_date
+        ? DateTime.fromISO(course.end_date)
+        : null
+      const currentDate = DateTime.local()
+
+      if (status === "Active" && courseEndDate && currentDate > courseEndDate) {
+        newStatus = "Ended"
+      } else if (
+        status === "Upcoming" &&
+        courseStartDate &&
+        currentDate >= courseStartDate
+      ) {
+        newStatus = "Active"
+      }
+
+      if (status === newStatus) {
+        return Promise.resolve()
+      }
+
+      console.log(
+        `Updated course ${course.name} from ${status} to ${newStatus}`,
+      )
+      const updatedCourse = await prisma.updateCourse({
+        where: {
+          id: course.id,
+        },
+        data: {
+          status: newStatus,
+        },
+      })
+      const msg: ProducerMessage = {
+        message: JSON.stringify(updatedCourse),
+        partition: null,
+        topic: "updated-course-status",
+      }
+      await kafkaProducer.queueProducerMessage(msg)
+    }),
+  )
+  await kafkaProducer.disconnect()
+}
+
+updateCourseStatuses()
