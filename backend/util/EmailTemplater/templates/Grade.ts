30b6820d361ec40605f6fa765d8ca8b4f7073de0
Daniel Koch <daniel.koch@helsinki.fi>
2020-04-16 15:55:15 +0300

    new paramter to email templater

---
+++ backend/util/EmailTemplater/templates/Grade.ts
@@ -0,0 +1,18 @@
+import Template from "../types/Template"
+import { prisma } from "../../../generated/prisma-client"
+
+export class Grade extends Template {
+  async resolve() {
+    const course = (
+      await prisma.courses({
+        where: { completion_email: { id: this.emailTemplate.id } },
+      })
+    )[0]
+    const grade = (
+      await prisma.completions({
+        where: { user: { id: this.user.id }, course: { id: course.id } },
+      })
+    )[0].grade
+    return `${grade}`
+  }
+}
