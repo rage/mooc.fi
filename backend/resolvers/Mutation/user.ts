4e03af4adbc8a17ab2c975728dcd2255c76c4cc9
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2020-05-22 12:37:31 +0300

    still missing the save functionality

--- backend/resolvers/Mutation/user.ts
+++ backend/resolvers/Mutation/user.ts
@@ -1,5 +1,5 @@
 import { PrismaObjectDefinitionBlock } from "nexus-prisma/dist/blocks/objectType"
-import { stringArg } from "nexus/dist"
+import { stringArg, booleanArg } from "nexus/dist"
 import { AuthenticationError } from "apollo-server-core"
 import { invalidate } from "../../services/redis"
 
@@ -37,8 +37,40 @@ const updateUserName = (t: PrismaObjectDefinitionBlock<"Mutation">) => {
   })
 }
 
+const updateResearchConsent = (t: PrismaObjectDefinitionBlock<"Mutation">) => {
+  t.field("updateResearchConsent", {
+    type: "User",
+    args: {
+      value: booleanArg({ required: true }),
+    },
+    resolve: (_, { value }, ctx) => {
+      const {
+        prisma,
+        user: currentUser,
+        request: {
+          headers: { authorization },
+        },
+      } = ctx
+
+      if (!currentUser) {
+        throw new AuthenticationError("not logged in")
+      }
+      const access_token = authorization.split(" ")[1]
+
+      invalidate("userdetails", `Bearer ${access_token}`)
+
+      return prisma.updateUser({
+        where: { id: currentUser.id },
+        data: {
+          research_consent: value,
+        },
+      })
+    },
+  })
+}
 const addUserMutations = (t: PrismaObjectDefinitionBlock<"Mutation">) => {
   updateUserName(t)
+  updateResearchConsent(t)
 }
 
 export default addUserMutations
