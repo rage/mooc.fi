5830ae2b4c6da25c4950b67136441c4ae74517a0
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2020-06-04 12:05:43 +0300

    added booleans for ects eligibility; completion creation

--- backend/types/CompletionArg.ts
+++ backend/types/CompletionArg.ts
@@ -5,6 +5,7 @@ const CompletionArg = inputObjectType({
   definition(t) {
     t.string("completion_id", { required: true })
     t.string("student_number", { required: true })
+    t.boolean("eligible_for_ects", { required: false })
   },
 })
 
