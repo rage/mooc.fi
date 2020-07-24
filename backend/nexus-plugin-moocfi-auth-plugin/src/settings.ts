2739260a27766e9676b600918d0cc7e1ec9765ac
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2020-07-24 21:36:55 +0300

    test if build broken just locally

--- backend/nexus-plugin-moocfi-auth-plugin/src/settings.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '../../node_modules/@prisma/client' // eh
-import * as redis from 'redis'
-
-export interface Settings {
-  prisma: PrismaClient
-  redisClient?: redis.RedisClient
-}
