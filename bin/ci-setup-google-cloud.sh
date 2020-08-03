6e10021a98ba3d459491385c74bfaaf4b61bac42
Henrik Nygren <nygrenh@gmail.com>
2020-08-03 16:36:44 +0300

    Cache on all branches

--- bin/ci-setup-google-cloud.sh
+++ bin/ci-setup-google-cloud.sh
@@ -1,15 +1,12 @@
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
+echo "Configuring Google Cloud"
+echo $GCLOUD_SERVICE_KEY >${HOME}/gcloud-service-key.json
+gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json
+gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
+gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
+echo "Configuring kubectl"
+gcloud --quiet container clusters get-credentials moocfi-cluster
+kubectl config set-context "$GOOGLE_CLUSTER_NAME" --namespace=moocfi
 
-if [[ "$BRANCH" == "master" ]]; then
-  echo "Configuring Google Cloud"
-  echo $GCLOUD_SERVICE_KEY >${HOME}/gcloud-service-key.json
-  gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json
-  gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
-  gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
-  echo "Configuring kubectl"
-  gcloud --quiet container clusters get-credentials moocfi-cluster
-  kubectl config set-context "$GOOGLE_CLUSTER_NAME" --namespace=moocfi
+echo "Logging in to docker"
+gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://eu.gcr.io
 
-  echo "Logging in to docker"
-  gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://eu.gcr.io
-fi
