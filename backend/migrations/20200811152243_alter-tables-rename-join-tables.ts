649e77ee31c3f7dc8e05fd57b5d6e1ce81f0afa2
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2020-08-11 15:38:33 +0300

    rename join tables

---
+++ backend/migrations/20200811152243_alter-tables-rename-join-tables.ts
@@ -0,0 +1,19 @@
+import * as Knex from "knex"
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(
+    `ALTER TABLE "_CourseToService" RENAME TO "_course_to_service";`,
+  )
+  await knex.raw(
+    `ALTER TABLE "_StudyModuleToCourse" RENAME TO "_study_module_to_course";`,
+  )
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.raw(
+    `ALTER TABLE "_course_to_service" RENAME TO "_CourseToService";`,
+  )
+  await knex.raw(
+    `ALTER TABLE "_study_module_to_course" RENAME TO "_StudyModuleToCourse";`,
+  )
+}
