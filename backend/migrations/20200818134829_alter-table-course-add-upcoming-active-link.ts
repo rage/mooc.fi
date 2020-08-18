f610ba49314c54a0af7d6ded3a4b9d5dcdefd7af
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2020-08-18 14:44:16 +0300

    upcoming course link not active by default; checkbox in course editor

---
+++ backend/migrations/20200818134829_alter-table-course-add-upcoming-active-link.ts
@@ -0,0 +1,19 @@
+import * as Knex from "knex"
+
+export async function up(knex: Knex): Promise<any> {
+  return knex.schema
+    .hasColumn("course", "upcoming_active_link")
+    .then((exists) => {
+      if (!exists) {
+        return knex.schema.alterTable("course", function (table) {
+          table.boolean("upcoming_active_link").defaultTo(false)
+        })
+      }
+    })
+}
+
+export async function down(knex: Knex): Promise<any> {
+  return knex.schema.alterTable("course", function (table) {
+    table.dropColumn("upcoming_active_link")
+  })
+}
