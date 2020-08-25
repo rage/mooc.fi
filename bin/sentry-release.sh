1d9695ab6eea016512e7bbd35be31860853dd9a5
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2020-08-25 16:29:02 +0300

    moved release script to backend build step

--- bin/sentry-release.sh
+++ bin/sentry-release.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 if [[ "$BRANCH" != "master" ]]; then
+  echo "Not on master branch, exiting"
   exit 0
 fi
 
@@ -11,6 +12,12 @@ SENTRY_RELEASE="moocfi-backend@$GIT_COMMIT_HASH"
 SENTRY_PROJECT="moocfi"
 SENTRY_ORG="moocfi"
 
+cd backend
+
 npx @sentry/cli releases --org $SENTRY_ORG new $SENTRY_RELEASE --project $SENTRY_PROJECT
-npx @sentry/cli releases --org $SENTRY_ORG -p $SENTRY_PROJECT files $SENTRY_RELEASE upload-sourcemaps ./backend/sourcemap
+npx @sentry/cli releases --org $SENTRY_ORG -p $SENTRY_PROJECT files $SENTRY_RELEASE upload-sourcemaps ./sourcemap
 npx @sentry/cli releases --org $SENTRY_ORG finalize $SENTRY_RELEASE
+
+cd ..
+
+echo "Created Sentry release $SENTRY_RELEASE"
