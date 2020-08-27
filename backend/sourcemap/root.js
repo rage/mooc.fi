f013c670ecc78baaba85c3aa660ddef832893a34
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2020-08-27 16:06:08 +0300

    remove sourcemap

--- backend/sourcemap/root.js
+++
@@ -1,5 +0,0 @@
-"use strict"
-exports.__esModule = true
-var rootdir = __dirname || process.cwd()
-exports["default"] = rootdir
-//# sourceMappingURL=root.js.map
--- backend/sourcemap/util/notEmpty.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-exports.__esModule = true
-exports.notEmpty = void 0
-function notEmpty(value) {
-  return value !== null && value !== undefined
-}
-exports.notEmpty = notEmpty
-//# sourceMappingURL=notEmpty.js.map
