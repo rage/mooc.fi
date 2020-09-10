16d17d0ffebd708d14400a0b0f9d4f8a0888f443
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2020-09-10 15:39:53 +0300

    remove meta from _document

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -50,10 +50,6 @@ class MyDocument extends Document {
       <html lang="fi" dir="ltr">
         <Head>
           <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
-          />
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link
             rel="shortcut icon"
