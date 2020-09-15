83e56030b5506efa47b2d999dbabe63a423f09f0
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2020-09-15 10:35:25 +0300

    copy/paste exerciseconsumer impl

--- backend/bin/kafkaConsumer/exerciseConsumer/kafkaConsumer.ts
+++ backend/bin/kafkaConsumer/exerciseConsumer/kafkaConsumer.ts
@@ -14,6 +14,7 @@ import { saveToDatabase } from "./saveToDB"
 import config from "../kafkaConfig"
 import { createKafkaConsumer } from "../common/kafkaConsumer"
 import { KafkaError } from "../../lib/errors"
+import { LibrdKafkaError } from "node-rdkafka"
 
 const TOPIC_NAME = [config.exercise_consumer.topic_name]
 
@@ -26,22 +27,35 @@ const consumer = createKafkaConsumer(logger)
 
 consumer.connect()
 
-consumer
-  .on("ready", () => {
-    consumer.subscribe(TOPIC_NAME)
-    consumer.consume()
-  })
-  .on("data", (message) =>
-    handleMessage<Message>({
-      kafkaMessage: message,
-      mutex,
-      logger,
-      consumer,
-      prisma,
-      MessageYupSchema,
-      saveToDatabase,
-    }),
-  )
+consumer.on("ready", () => {
+  consumer.subscribe(TOPIC_NAME)
+  const consumerImpl = async (error: LibrdKafkaError, messages: any) => {
+    if (error) {
+      logger.error(new KafkaError("Error while consuming", error))
+      process.exit(-1)
+    }
+    if (messages.length > 0) {
+      await handleMessage<Message>({
+        kafkaMessage: messages[0],
+        mutex,
+        logger,
+        consumer,
+        prisma,
+        MessageYupSchema,
+        saveToDatabase,
+      })
+      setImmediate(() => {
+        consumer.consume(1, consumerImpl)
+      })
+    } else {
+      setTimeout(() => {
+        consumer.consume(1, consumerImpl)
+      }, 10)
+    }
+  }
+  consumer.consume(1, consumerImpl)
+})
+
 consumer.on("event.error", (error) => {
   logger.error(new KafkaError("Error", error))
   throw error
