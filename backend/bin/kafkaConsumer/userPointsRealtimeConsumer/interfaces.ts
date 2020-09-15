499243e475e64530335049a1813c6d0c0a66f24e
Henrik Nygren <nygrenh@gmail.com>
2020-09-15 10:08:56 +0300

    Add user-points realtime implementation

---
+++ backend/bin/kafkaConsumer/userPointsRealtimeConsumer/interfaces.ts
@@ -0,0 +1,11 @@
+export interface Message {
+  timestamp: string
+  exercise_id: string
+  n_points: number | null
+  completed: boolean
+  user_id: number
+  course_id: string
+  service_id: string
+  required_actions: string[]
+  message_format_version: number
+}
