65f78a3af9f69fb7b4a025ca43d1ccbdb5bed60c
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2020-09-29 12:19:11 +0300

    moved extra progress stuff to own field; computed project completion field

---
+++ backend/migrations/20200929115223_alter-table-user-course-progress-extra.ts
@@ -0,0 +1,15 @@
+import * as Knex from "knex"
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(`
+    ALTER TABLE user_course_progress 
+      ADD COLUMN IF NOT EXISTS extra JSON;  
+  `)
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.raw(`
+    ALTER TABLE user_course_progress
+      DROP COLUMN extra;
+  `)
+}
