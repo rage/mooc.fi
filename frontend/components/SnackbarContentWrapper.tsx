a604b4a0ee8d5c841e3b217b97cf7b1ee252b909
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2020-10-13 14:40:02 +0300

    forgot to copy the downloaded env to right place

--- frontend/components/SnackbarContentWrapper.tsx
+++ frontend/components/SnackbarContentWrapper.tsx
@@ -24,11 +24,8 @@ const StyledSnackbarContent = styled(SnackbarContent)<
 >`
   ${(props) => props.type === "info" && `background-color: #3f51b5;`}
   ${(props) => props.type === "success" && `background-color: #43a047;`}
-    ${(
-    props,
-  ) => props.type === "error" && `background-color: #d32f2f;`}
-    ${(props) =>
-    props.type === "warning" && `background-color: #ffa000;`}
+    ${(props) => props.type === "error" && `background-color: #d32f2f;`}
+    ${(props) => props.type === "warning" && `background-color: #ffa000;`}
 `
 
 const Span = styled.span``
