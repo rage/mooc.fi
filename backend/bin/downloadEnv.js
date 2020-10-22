db11d5e2f09de0f39f33a09248040f20b8d80748
Ren <nsoharab@gmail.com>
2020-10-22 22:06:43 +0300

    updated env config

--- backend/bin/downloadEnv.js
+++ backend/bin/downloadEnv.js
@@ -15,5 +15,5 @@ function download(filename, url) {
 console.log("Downloading BAI course env")
 download(
   path.join(__dirname, "/kafkaConsumer/common/userCourseProgress/env.json"),
-  "https://gist.githubusercontent.com/Technopathic/2443135b588895ff2b21631393eacdb6/raw/97f64a9926aba6313deb8dd85a1b00be9288c99b/env.json",
+  "https://gist.githubusercontent.com/Technopathic/2443135b588895ff2b21631393eacdb6/raw/49e796efd23a8bc1f72b24d5f3b8b3f90667c64f/env.json",
 )
