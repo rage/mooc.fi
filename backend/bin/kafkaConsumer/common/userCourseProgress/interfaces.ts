47594ed6933e599af349fcf87e535000c23cef20
Mikko Pyykkö <mipyykko@gmail.com>
2020-10-22 12:02:38 +0300

    break down user course progress a bit; retry user fetch in getUser before erroring

--- backend/bin/kafkaConsumer/common/userCourseProgress/interfaces.ts
+++ backend/bin/kafkaConsumer/common/userCourseProgress/interfaces.ts
@@ -13,3 +13,29 @@ export interface PointsByGroup {
   n_points: number
   progress: number
 }
+
+export interface ServiceProgressPartType {
+  max_points: number
+  n_points: number
+  group: string
+  progress: number
+}
+
+export interface ServiceProgressType extends Array<ServiceProgressPartType> {
+  [index: number]: ServiceProgressPartType
+}
+
+export interface TierProgress {
+  tier: number
+  n_points: number
+  max_points: number
+  progress: number
+}
+
+export interface ExerciseCompletionPart {
+  course_id: string
+  exercise_id: string
+  max_points?: number
+  n_points?: number
+  custom_id?: string
+}
