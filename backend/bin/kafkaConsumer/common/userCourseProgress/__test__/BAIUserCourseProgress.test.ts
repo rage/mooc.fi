f0161ae6a3186007105709aecd7a7b4007fa4993
Mikko Pyykkö <mipyykko@gmail.com>
2020-10-23 14:04:58 +0300

    some basic tests with progress

---
+++ backend/bin/kafkaConsumer/common/userCourseProgress/__test__/BAIUserCourseProgress.test.ts
@@ -0,0 +1,320 @@
+import {
+  getBAIProgress,
+  getProgress,
+  getRules,
+  getTierInfo,
+  getTierProgress,
+  TierInfo,
+  TierProgressMap,
+} from "../generateBAIUserCourseProgress"
+import { User, Course } from "@prisma/client"
+import { ExerciseCompletionPart } from "../interfaces"
+
+const env = require("../env.json")
+
+describe("calculates tier progress correctly", () => {
+  test("skips exercises not found", () => {
+    const data: ExerciseCompletionPart[] = [
+      {
+        course_id: "test",
+        exercise_id: "fake1",
+        custom_id: "fake",
+        max_points: 1,
+        n_points: 0,
+      },
+      {
+        course_id: "test",
+        exercise_id: "fake2",
+        custom_id: env.EX_ONE_BEGINNER,
+        max_points: 2,
+        n_points: 1,
+      },
+    ]
+
+    const { tierProgressMap, tierProgress, progress } = getTierProgress(data)
+
+    expect(tierProgressMap).toEqual({
+      1: {
+        tier: 1,
+        max_points: 2,
+        n_points: 1,
+        progress: 0.5,
+      },
+    })
+    expect(tierProgress).toEqual([
+      {
+        group: "1",
+        tier: 1,
+        max_points: 2,
+        n_points: 1,
+        progress: 0.5,
+      },
+    ])
+    expect(progress).toEqual({
+      total_n_points: 1,
+      total_max_points: 2,
+    })
+  })
+
+  test("gets highest tier and picks highest score", () => {
+    const data: ExerciseCompletionPart[] = [
+      {
+        course_id: "test",
+        exercise_id: "fake",
+        custom_id: env.EX_ONE_BEGINNER,
+        max_points: 2,
+        n_points: 2,
+      },
+      {
+        course_id: "test",
+        exercise_id: "fake2",
+        custom_id: env.EX_ONE_INTERMEDIATE_ID,
+        max_points: 2,
+        n_points: 1,
+      },
+      {
+        course_id: "test",
+        exercise_id: "fake3",
+        custom_id: env.EX_TWO_INTERMEDIATE_ID,
+        max_points: 4,
+        n_points: 1,
+      },
+      {
+        course_id: "test",
+        exercise_id: "fake4",
+        custom_id: env.EX_TWO_ADVANCED_ID,
+        max_points: 4,
+        n_points: 2,
+      },
+    ]
+
+    const { tierProgress, progress } = getTierProgress(data)
+
+    expect(tierProgress).toEqual([
+      {
+        group: "1",
+        tier: 2,
+        max_points: 2,
+        n_points: 2,
+        progress: 1,
+      },
+      {
+        group: "2",
+        tier: 3,
+        max_points: 4,
+        n_points: 2,
+        progress: 0.5,
+      },
+    ])
+    expect(progress).toEqual({
+      total_n_points: 4,
+      total_max_points: 6,
+    })
+  })
+
+  test("does not care about max_points === 0", () => {
+    const data: ExerciseCompletionPart[] = [
+      {
+        course_id: "test",
+        exercise_id: "fake",
+        custom_id: env.EX_ONE_BEGINNER,
+        max_points: 0,
+        n_points: 0,
+      },
+      {
+        course_id: "test",
+        exercise_id: "fake",
+        custom_id: env.EX_TWO_BEGINNER,
+        max_points: 1,
+        n_points: 1,
+      },
+    ]
+
+    const { tierProgressMap, progress } = getTierProgress(data)
+
+    expect(tierProgressMap).toEqual({
+      1: {
+        tier: 1,
+        max_points: 0,
+        n_points: 0,
+        progress: 0,
+      },
+      2: {
+        tier: 1,
+        max_points: 1,
+        n_points: 1,
+        progress: 1,
+      },
+    })
+
+    expect(progress).toEqual({
+      total_n_points: 1,
+      total_max_points: 1,
+    })
+  })
+})
+
+describe("gets the rules right", () => {
+  test("not enough points", () => {
+    const {
+      hasEnoughPoints,
+      hasEnoughExerciseCompletions,
+      hasBasicRule,
+    } = getRules({
+      totalExerciseCompletions: 10,
+      n_points: 6,
+      points_needed: 7,
+      exercise_completions_needed: 6,
+    })
+
+    expect(hasEnoughPoints).toBe(false)
+    expect(hasEnoughExerciseCompletions).toBe(true)
+    expect(hasBasicRule).toBe(false)
+  })
+
+  test("not enough exercise completions", () => {
+    const {
+      hasEnoughPoints,
+      hasEnoughExerciseCompletions,
+      hasBasicRule,
+    } = getRules({
+      totalExerciseCompletions: 6,
+      n_points: 10,
+      points_needed: 10,
+      exercise_completions_needed: 10,
+    })
+
+    expect(hasEnoughPoints).toBe(true)
+    expect(hasEnoughExerciseCompletions).toBe(false)
+    expect(hasBasicRule).toBe(false)
+  })
+
+  test("hunky dory", () => {
+    const {
+      hasEnoughPoints,
+      hasEnoughExerciseCompletions,
+      hasBasicRule,
+    } = getRules({
+      totalExerciseCompletions: 20,
+      n_points: 15,
+      points_needed: 10,
+      exercise_completions_needed: 20,
+    })
+
+    expect(hasEnoughPoints).toBe(true)
+    expect(hasEnoughExerciseCompletions).toBe(true)
+    expect(hasBasicRule).toBe(true)
+  })
+})
+
+describe("gets tier info", () => {
+  const tierProgressMap: TierProgressMap = {
+    1: {
+      tier: 1,
+      max_points: 1,
+      n_points: 1,
+      progress: 1,
+    },
+    2: {
+      tier: 2,
+      max_points: 2,
+      n_points: 1,
+      progress: 0.5,
+    },
+    3: {
+      tier: 3,
+      max_points: 5,
+      n_points: 2,
+      progress: 0.4,
+    },
+  }
+
+  test("no basic rule", () => {
+    const { tierInfo, highestTier } = getTierInfo({
+      tierProgressMap,
+      hasBasicRule: false,
+      required_by_tier: { 1: 3, 2: 2, 3: 1 },
+    })
+
+    Object.values(tierInfo).forEach((i) => expect(i.hasTier).toBe(false))
+
+    expect(highestTier).toEqual(0)
+  })
+
+  test("has beginner tier", () => {
+    const { tierInfo, highestTier } = getTierInfo({
+      tierProgressMap,
+      hasBasicRule: true,
+      required_by_tier: { 1: 2, 2: 3, 3: 2 },
+    })
+
+    expect(tierInfo).toEqual({
+      beginner: {
+        hasTier: true,
+        missingFromTier: 0,
+        exerciseCompletions: 3,
+      },
+      intermediate: {
+        hasTier: false,
+        missingFromTier: 1,
+        exerciseCompletions: 2,
+      },
+      advanced: {
+        hasTier: false,
+        missingFromTier: 1,
+        exerciseCompletions: 1,
+      },
+    })
+
+    expect(highestTier).toEqual(1)
+  })
+})
+
+describe("gets progress", () => {
+  const tierProgressMap: TierProgressMap = {
+    1: {
+      tier: 1,
+      max_points: 1,
+      n_points: 1,
+      progress: 1,
+    },
+  }
+
+  const tierInfo: TierInfo = {
+    beginner: {
+      hasTier: true,
+      missingFromTier: 0,
+      exerciseCompletions: 3,
+    },
+  }
+
+  test("successfully", () => {
+    const progress = getProgress({
+      n_points: 4,
+      max_points: 8,
+      tierInfo,
+      tierProgressMap,
+      projectCompletion: true,
+      highestTier: 1,
+      totalExerciseCompletions: 3,
+    })
+
+    expect(progress).toEqual({
+      progress: [
+        {
+          group: "total",
+          max_points: 8,
+          n_points: 4,
+          progress: 0.5,
+        },
+      ],
+      extra: {
+        tiers: tierInfo,
+        exercises: tierProgressMap,
+        projectCompletion: true,
+        highestTier: 1,
+        totalExerciseCompletions: 3,
+      },
+    })
+  })
+})
--- backend/bin/kafkaConsumer/common/userCourseProgress/generateBAIUserCourseProgress.ts
+++ backend/bin/kafkaConsumer/common/userCourseProgress/generateBAIUserCourseProgress.ts
@@ -18,6 +18,7 @@ import {
 } from "./userFunctions"
 import { ExerciseCompletionPart, TierProgress } from "./interfaces"
 import { range } from "lodash"
+import { BooleanSchema } from "yup"
 
 const prisma = prismaClient()
 
@@ -69,12 +70,15 @@ export const checkBAICompletion = async (
     [{ course_id, exercise_id, n_points }...] for all the tiers
   */
 
+  logger?.info("Getting project completion")
+  const projectCompletion = await checkBAIProjectCompletion(user)
+
   logger?.info("Getting BAI course progress")
-  const { progress: newProgress, highestTier } = await getBAIProgress(
-    user,
-    handlerCourse,
+  const { progress: newProgress, highestTier } = getBAIProgress(
+    projectCompletion,
     exerciseCompletionsForCourses,
   )
+
   const existingProgress = await prisma.userCourseProgress.findMany({
     where: {
       course_id: handlerCourse.id,
@@ -118,10 +122,22 @@ export const checkBAICompletion = async (
   })
 }
 
-const getBAIProgress = async (
-  user: User,
-  // @ts-ignore: not needed now
-  handlerCourse: Course,
+interface TierProgressGroup {
+  group: string
+  tier: number
+  max_points: number
+  n_points: number
+  progress: number
+}
+
+export type TierProgressMap = Record<number, TierProgress>
+
+interface Progress {
+  total_n_points: number
+  total_max_points: number
+}
+
+export const getTierProgress = (
   exerciseCompletionsForCourses: ExerciseCompletionPart[],
 ) => {
   const tierProgressMap = exerciseCompletionsForCourses.reduce((acc, curr) => {
@@ -141,18 +157,20 @@ const getBAIProgress = async (
         progress: n_points / (max_points || 1),
       },
     }
-  }, {} as Record<number, TierProgress>)
+  }, {} as TierProgressMap)
   /*
     [exercise #]: { tier, n_points, max_points } -- what's the maximum tier completed and
       what's the highest amount of points received, not necessarily from maximum tier
     ...
   */
-  const tierProgress = Object.entries(tierProgressMap).map(([key, value]) => ({
-    group: key,
-    ...value,
-  }))
+  const tierProgress: TierProgressGroup[] = Object.entries(tierProgressMap).map(
+    ([key, value]) => ({
+      group: key,
+      ...value,
+    }),
+  )
 
-  const progress = Object.values(tierProgressMap).reduce(
+  const progress: Progress = Object.values(tierProgressMap).reduce(
     (acc, curr) => ({
       total_n_points: acc.total_n_points + curr.n_points,
       total_max_points: acc.total_max_points + curr.max_points,
@@ -160,12 +178,58 @@ const getBAIProgress = async (
     { total_n_points: 0, total_max_points: 0 },
   )
 
-  const totalExerciseCompletions = Object.keys(tierProgress).length
-  const hasEnoughPoints = progress.total_n_points >= pointsNeeded //(handlerCourse.points_needed ?? 9999999)
+  return {
+    tierProgressMap,
+    tierProgress,
+    progress,
+  }
+}
+
+interface Rules {
+  totalExerciseCompletions: number
+  n_points: number
+  points_needed: number
+  exercise_completions_needed: number
+}
+
+export const getRules = ({
+  totalExerciseCompletions,
+  n_points,
+  points_needed,
+  exercise_completions_needed,
+}: Rules) => {
+  const hasEnoughPoints = n_points >= points_needed //(handlerCourse.points_needed ?? 9999999)
   const hasEnoughExerciseCompletions =
-    totalExerciseCompletions >= exerciseCompletionsNeeded //(handlerCourse.exercise_completions_needed ?? 0)
+    totalExerciseCompletions >= exercise_completions_needed //(handlerCourse.exercise_completions_needed ?? 0)
   const hasBasicRule = hasEnoughPoints && hasEnoughExerciseCompletions
 
+  return {
+    hasEnoughPoints,
+    hasEnoughExerciseCompletions,
+    hasBasicRule,
+  }
+}
+
+interface GetTierInfo {
+  tierProgressMap: TierProgressMap
+  hasBasicRule: boolean
+  required_by_tier: Record<number, number>
+}
+
+export type TierInfo = Record<
+  string,
+  {
+    hasTier: boolean
+    missingFromTier: number
+    exerciseCompletions: number
+  }
+>
+
+export const getTierInfo = ({
+  tierProgressMap,
+  hasBasicRule,
+  required_by_tier,
+}: GetTierInfo) => {
   const tierCompletions = range(1, 4).reduce(
     (acc, tier) => ({
       ...acc,
@@ -181,14 +245,14 @@ const getBAIProgress = async (
 
   const hasTier: Record<number, boolean> = {
     1: hasBasicRule,
-    2: hasBasicRule && tierCompletions[2] >= requiredByTier[2],
-    3: hasBasicRule && tierCompletions[3] >= requiredByTier[3],
+    2: hasBasicRule && tierCompletions[2] >= required_by_tier[2],
+    3: hasBasicRule && tierCompletions[3] >= required_by_tier[3],
   }
 
   const missingFromTier = range(1, 4).reduce(
     (acc, tier) => ({
       ...acc,
-      [tier]: Math.max(0, requiredByTier[tier] - tierCompletions[tier]),
+      [tier]: Math.max(0, required_by_tier[tier] - tierCompletions[tier]),
     }),
     {} as Record<number, number>,
   )
@@ -201,7 +265,7 @@ const getBAIProgress = async (
     0,
   )
 
-  const tierInfo = Object.keys(BAItiers)
+  const tierInfo: TierInfo = Object.keys(BAItiers)
     .map(Number)
     .reduce(
       (acc, tier) => ({
@@ -215,15 +279,38 @@ const getBAIProgress = async (
       {},
     )
 
-  const projectCompletion = await checkBAIProjectCompletion(user)
-  const pointsProgress =
-    (progress.total_n_points || 0) / (progress.total_max_points || 1)
+  return {
+    tierInfo,
+    highestTier,
+  }
+}
+
+interface GetProgress {
+  n_points?: number | null
+  max_points?: number | null
+  tierInfo: TierInfo
+  tierProgressMap: TierProgressMap
+  projectCompletion: Boolean
+  highestTier: number
+  totalExerciseCompletions: number
+}
+
+export const getProgress = ({
+  n_points,
+  max_points,
+  tierInfo,
+  tierProgressMap,
+  projectCompletion,
+  highestTier,
+  totalExerciseCompletions,
+}: GetProgress) => {
+  const pointsProgress = (n_points || 0) / (max_points || 1)
   const newProgress = {
     progress: [
       {
         group: "total",
-        max_points: progress.total_max_points,
-        n_points: progress.total_n_points,
+        max_points,
+        n_points,
         progress: isNaN(pointsProgress) ? 0 : pointsProgress,
       },
     ],
@@ -235,6 +322,42 @@ const getBAIProgress = async (
       totalExerciseCompletions,
     },
   }
+
+  return newProgress
+}
+
+export const getBAIProgress = (
+  projectCompletion: boolean,
+  exerciseCompletionsForCourses: ExerciseCompletionPart[],
+) => {
+  const { progress, tierProgress, tierProgressMap } = getTierProgress(
+    exerciseCompletionsForCourses,
+  )
+
+  const totalExerciseCompletions = Object.keys(tierProgress).length
+
+  const { hasBasicRule } = getRules({
+    n_points: progress.total_n_points,
+    totalExerciseCompletions,
+    points_needed: pointsNeeded,
+    exercise_completions_needed: exerciseCompletionsNeeded,
+  })
+
+  const { tierInfo, highestTier } = getTierInfo({
+    tierProgressMap,
+    hasBasicRule,
+    required_by_tier: requiredByTier,
+  })
+
+  const newProgress = getProgress({
+    n_points: progress.total_n_points,
+    max_points: progress.total_max_points,
+    tierInfo,
+    tierProgressMap,
+    projectCompletion,
+    highestTier,
+    totalExerciseCompletions,
+  })
 
   return {
     progress: newProgress,
