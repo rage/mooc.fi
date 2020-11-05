e22a8cad913ffc43a7f1a8bc83e18f439ab39b93
Mikko Pyykkö <mipyykko@gmail.com>
2020-11-05 16:34:31 +0200

    dropping the schema too early

--- backend/bin/create-db-for-tests.ts
+++ backend/bin/create-db-for-tests.ts
@@ -1,9 +1,9 @@
 const connection = {
   host: "localhost",
-  port: 5678,
-  user: "prisma",
-  password: "prisma",
-  database: "prisma",
+  port: 5432,
+  user: "postgres",
+  password: "postgres",
+  // database: "testing",
 }
 
 const knex = require("knex")({
@@ -11,4 +11,14 @@ const knex = require("knex")({
   connection,
 })
 
-knex.raw("CREATE DATABASE testing;")
+const createDb = async () => {
+  try {
+    await knex.raw("CREATE DATABASE testing;")
+  } catch (e) {
+    console.error(`Error creating test db: ${e}`)
+  }
+}
+
+createDb()
+  .then(() => process.exit(0))
+  .catch(() => process.exit(1))
