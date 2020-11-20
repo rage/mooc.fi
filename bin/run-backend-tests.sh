3e8ae7d1516bb045f0e87f1f8126ccb23d99ffad
Mikko Pyykkö <mipyykko@gmail.com>
2020-11-20 17:50:54 +0200

    set up coverage artifacts

--- bin/run-backend-tests.sh
+++ bin/run-backend-tests.sh
@@ -13,13 +13,20 @@ fi
 
 TAG="eu.gcr.io/moocfi/moocfi-backend:build-$REV"
 
+
 cd backend
 
 echo "Running tests"
 
-docker run --env NODE_ENV=test --env PGPASSWORD=prisma --env \
-  LD_PRELOAD=/app/node_modules/sharp/vendor/lib/libz.so --network host \
-  --rm "eu.gcr.io/moocfi/moocfi-backend:build-$TAG" \
-  /bin/bash -c "npm run create-test-db; npm run test -- --runInBand --ci --coverage"
+docker run --env NODE_ENV=test --env PGPASSWORD=prisma \
+  --env LD_PRELOAD=/app/node_modules/sharp/vendor/lib/libz.so \
+  --network host \
+  --name test "$TAG" \
+  /bin/bash -c "npm run create-test-db; npm run test -- --runInBand --ci --coverage --reporters=default --reporters=jest-junit" 
+
+echo "Copying coverage metadata"
+
+docker cp test:/app/coverage coverage
+docker rm test
 
 cd ..
