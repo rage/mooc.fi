830bb50d3641fabea40c33b8f3d9fdf45c2327ef
Mikko Pyykkö <mipyykko@gmail.com>
2020-12-01 13:26:29 +0200

    fix (?) sentry upload

--- backend/graphql/StudyModule/index.ts
+++ backend/graphql/StudyModule/index.ts
@@ -1,4 +1,4 @@
-// generated Sat Nov 07 2020 16:14:08 GMT+0200 (Eastern European Standard Time)
+// generated Tue Dec 01 2020 13:06:03 GMT+0200 (Eastern European Standard Time)
 
 export * from "./input"
 export * from "./model"
