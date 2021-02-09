fd695fffffea5b596ff6eafed3bf45b70ec70cd9
Ren <nsoharab@gmail.com>
2021-02-09 11:35:41 +0200

    Added new auth functions

---
+++ backend/migrations/20210209111701_alter-table-user-add-reset.ts
@@ -0,0 +1,19 @@
+import * as Knex from "knex";
+
+
+export async function up(knex: Knex): Promise<any> {
+    return knex.schema.hasColumn("user", "password_reset").then((exists) => {
+        if (!exists) {
+            return knex.schema.alterTable("user", function (table) {
+                table.string("password_reset", 255)
+            })
+        }
+    })
+}
+
+
+export async function down(knex: Knex): Promise<any> {
+    return knex.schema.alterTable("user", function (table) {
+        table.dropColumn("password_reset")
+    })
+}
