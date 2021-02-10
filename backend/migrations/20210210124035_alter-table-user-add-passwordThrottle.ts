6f1968732a806d1982b189d2205819990226eeb1
Ren <nsoharab@gmail.com>
2021-02-10 13:42:44 +0200

    Adding password throttling to moocfi signin

---
+++ backend/migrations/20210210124035_alter-table-user-add-passwordThrottle.ts
@@ -0,0 +1,19 @@
+import * as Knex from "knex";
+
+
+export async function up(knex: Knex): Promise<any> {
+    return knex.schema.hasColumn("user", "passwordThrottle").then((exists) => {
+        if (!exists) {
+            return knex.schema.alterTable("user", function (table) {
+                table.json("passwordThrottle")
+            })
+        }
+    })
+}
+
+
+export async function down(knex: Knex): Promise<any> {
+    return knex.schema.alterTable("user", function (table) {
+        table.dropColumn("passwordThrottle")
+    })
+}
