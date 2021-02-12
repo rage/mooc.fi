17c9874d174969f93fe001e8719834fb16ad7b41
Mikko Pyykkö <mipyykko@gmail.com>
2021-02-12 16:05:51 +0200

    POC user points view with exercises, completions etc.

--- frontend/pages/[lng]/users/[id]/points.tsx
+++ frontend/pages/[lng]/users/[id]/points.tsx
@@ -1,12 +1,31 @@
-import { gql } from "@apollo/client"
+import { gql, useQuery } from "@apollo/client"
 import withAdmin from "/lib/with-admin"
+import {
+  UserPointsList,
+  UserPointsList_user_exercise_completions,
+  UserPointsList_user_user_course_progresses,
+} from "/static/types/generated/UserPointsList"
+import { useQueryParameter } from "/util/useQueryParameter"
+import { useEffect, useState } from "react"
+import { CircularProgress } from "@material-ui/core"
+import { sortBy } from "lodash"
+import CourseEntry from "/components/Dashboard/Users/Points/CourseEntry"
+import Container from "/components/Container"
 
-// @ts-ignore: nothing here yet
+// TODO: might as well query completions (and registered)
 const UserPointsQuery = gql`
-  query UserPointsList($id: ID!) {
-    user(id: $id) {
+  query UserPointsList($upstream_id: Int) {
+    user(upstream_id: $upstream_id) {
       id
       username
+      user_course_progresses {
+        id
+        course_id
+        max_points
+        n_points
+        progress
+        extra
+      }
       exercise_completions {
         id
         created_at
@@ -15,26 +34,86 @@ const UserPointsQuery = gql`
         attempted
         completed
         timestamp
+        n_points
         exercise_completion_required_actions {
           id
           value
         }
         exercise {
           id
+          name
           custom_id
           course {
             id
             name
           }
-          name
+          part
+          section
+          max_points
         }
       }
     }
   }
 `
 
+interface DataPerCourse {
+  progress?: UserPointsList_user_user_course_progresses
+  exerciseCompletions: UserPointsList_user_exercise_completions[]
+}
+
 function UserPoints() {
-  return <div>nothing</div>
+  const id = useQueryParameter("id")
+  const [groupedData, setGroupedData] = useState<
+    [string, DataPerCourse][] | null
+  >(null)
+  const { loading, /* error, */ data } = useQuery<UserPointsList>(
+    UserPointsQuery,
+    { variables: { upstream_id: Number(id) } },
+  )
+
+  useEffect(() => {
+    console.log("data", data)
+    // TODO: probably a more readable solution if it gets complicated
+    const dataPerCourse =
+      data?.user?.exercise_completions?.reduce<Record<string, DataPerCourse>>(
+        (acc, curr) => ({
+          ...acc,
+          ...(curr?.exercise?.course?.id
+            ? {
+                [curr.exercise.course.id]: {
+                  progress: data?.user?.user_course_progresses?.find(
+                    (p) => p.course_id === curr.exercise.course.id,
+                  ),
+                  exerciseCompletions: sortBy(
+                    (
+                      acc[curr.exercise.course.id]?.exerciseCompletions ?? []
+                    ).concat(curr),
+                    ["exercise.part", "exercise.section", "exercise.name"],
+                  ),
+                },
+              }
+            : {}),
+        }),
+        {},
+      ) ?? {}
+    setGroupedData(
+      sortBy(
+        Object.entries(dataPerCourse),
+        ([_, value]) =>
+          value?.exerciseCompletions?.[0].exercise?.course?.name ?? "",
+      ),
+    )
+  }, [data])
+
+  return (
+    <Container>
+      {loading || !groupedData ? (
+        <CircularProgress />
+      ) : (
+        groupedData.map(([id, data]) => <CourseEntry key={id} data={data} />)
+      )}
+    </Container>
+  )
 }
 
 export default withAdmin(UserPoints)
---
+++ frontend/static/types/generated/UserPointsList.ts
@@ -0,0 +1,70 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: UserPointsList
+// ====================================================
+
+export interface UserPointsList_user_user_course_progresses {
+  __typename: "UserCourseProgress"
+  id: string
+  course_id: string | null
+  progress: (any | null)[] | null
+  extra: any | null
+}
+
+export interface UserPointsList_user_exercise_completions_exercise_completion_required_actions {
+  __typename: "ExerciseCompletionRequiredAction"
+  id: string
+  value: string
+}
+
+export interface UserPointsList_user_exercise_completions_exercise_course {
+  __typename: "Course"
+  id: string
+  name: string
+}
+
+export interface UserPointsList_user_exercise_completions_exercise {
+  __typename: "Exercise"
+  id: string
+  name: string | null
+  custom_id: string
+  course: UserPointsList_user_exercise_completions_exercise_course | null
+  part: number | null
+  section: number | null
+  max_points: number | null
+}
+
+export interface UserPointsList_user_exercise_completions {
+  __typename: "ExerciseCompletion"
+  id: string
+  created_at: any | null
+  updated_at: any | null
+  n_points: number | null
+  attempted: boolean | null
+  completed: boolean | null
+  timestamp: any
+  exercise_completion_required_actions: UserPointsList_user_exercise_completions_exercise_completion_required_actions[]
+  exercise: UserPointsList_user_exercise_completions_exercise | null
+}
+
+export interface UserPointsList_user {
+  __typename: "User"
+  id: string
+  username: string
+  user_course_progresses: UserPointsList_user_user_course_progresses[]
+  exercise_completions:
+    | (UserPointsList_user_exercise_completions | null)[]
+    | null
+}
+
+export interface UserPointsList {
+  user: UserPointsList_user | null
+}
+
+export interface UserPointsListVariables {
+  upstream_id?: number | null
+}
