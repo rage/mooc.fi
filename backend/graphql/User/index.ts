aea0267e9fd0990cb69f1c6a7990640b84991c8e
Mikko Pyykkö <mipyykko@gmail.com>
2021-03-18 12:55:33 +0200

    course statistics wip

--- backend/graphql/User/index.ts
+++ backend/graphql/User/index.ts
@@ -1,4 +1,4 @@
-// generated Wed Feb 17 2021 19:08:35 GMT+0200 (GMT+02:00)
+// generated Thu Mar 18 2021 12:01:24 GMT+0200 (GMT+02:00)
 
 export * from "./input"
 export * from "./model"
---
+++ frontend/pages/[lng]/courses/[slug]/statistics.tsx
@@ -0,0 +1,9 @@
+import withAdmin from "/lib/with-admin"
+
+function CourseStatistics() {
+  return (
+    <div />
+  )
+} 
+
+export default withAdmin(CourseStatistics)
