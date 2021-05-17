45f80e4f8a88ed6e36bacd24e0d7988f0c2c76f5
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2021-05-17 16:06:32 +0300

    Add composite index for email_delivery

---
+++ backend/migrations/20210517160134_alter-table-add-email-delivery-composite-index.ts
@@ -0,0 +1,13 @@
+import { Knex } from "knex"
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.raw(
+    `CREATE INDEX IF NOT EXISTS "email_delivery.user_id_email_template_id" ON email_delivery USING btree(user_id, email_template_id);`,
+  )
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.raw(
+    `DROP INDEX IF EXISTS "cmail_delivery.user_id_email_template_id;`,
+  )
+}
