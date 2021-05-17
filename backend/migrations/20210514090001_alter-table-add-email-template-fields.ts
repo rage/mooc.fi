ee9251be75191b844463723d09ccb114398847bf
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2021-05-17 09:58:58 +0300

    Add fkey && prisma schema

--- backend/migrations/20210514090001_alter-table-add-email-template-fields.ts
+++ backend/migrations/20210514090001_alter-table-add-email-template-fields.ts
@@ -13,6 +13,14 @@ export async function up(knex: Knex): Promise<void> {
   await knex.raw(`ALTER TABLE email_template
     ADD COLUMN IF NOT EXISTS type STRING;
   `)
+  try {
+    await knex.transaction(async (trx) => {
+      await trx.raw(`ALTER TABLE ONLY email_template
+        ADD CONSTRAINT course_threshold_email_fkey FOREIGN KEY (triggered_automatically_by_course) REFERENCES course(id) ON DELETE SET NULL;`)
+    })
+  } catch {
+    console.log("Error adding foreign key constraints.")
+  }
 }
 
 export async function down(knex: Knex): Promise<void> {
