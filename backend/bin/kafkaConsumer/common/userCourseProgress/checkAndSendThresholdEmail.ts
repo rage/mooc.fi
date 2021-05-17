e8526357108257e69c5b93cddad36e432869be0f
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2021-05-17 15:38:47 +0300

    Check that templates exists

--- backend/bin/kafkaConsumer/common/userCourseProgress/checkAndSendThresholdEmail.ts
+++ backend/bin/kafkaConsumer/common/userCourseProgress/checkAndSendThresholdEmail.ts
@@ -19,14 +19,16 @@ export const checkAndSendThresholdEmail = async ({
   combinedUserCourseProgress,
   context,
 }: Props) => {
-  const emailTemplates = await context.prisma.emailTemplate.findMany({
-    where: {
-      triggered_automatically_by_course_id: course.id,
-      template_type: "threshold",
+  const courseEmailThresholdTemplates = await context.prisma.emailTemplate.findMany(
+    {
+      where: {
+        triggered_automatically_by_course_id: course.id,
+        template_type: "threshold",
+      },
     },
-  })
+  )
 
-  const templatesThatFulfill = emailTemplates
+  const templatesThatFulfillPoints = courseEmailThresholdTemplates
     .sort((a, b) => (b.points_threshold ?? 0) - (a.points_threshold ?? 0))
     .filter((et) => {
       return (
@@ -35,25 +37,27 @@ export const checkAndSendThresholdEmail = async ({
       )
     })
 
-  // Send only for the highest fulfilled?
-  const mailSent = await context.prisma.emailDelivery.findFirst({
-    where: {
-      email_template_id: templatesThatFulfill[0].id,
-      user_id: user.id,
-    },
-  })
-
-  if (!mailSent) {
-    await context.prisma.emailDelivery.create({
-      data: {
-        id: uuidv4(),
-        created_at: new Date(),
-        updated_at: new Date(),
+  if (templatesThatFulfillPoints.length !== 0) {
+    // Send only for the highest fulfilled?
+    const mailSent = await context.prisma.emailDelivery.findFirst({
+      where: {
+        email_template_id: templatesThatFulfillPoints[0].id,
         user_id: user.id,
-        email_template_id: templatesThatFulfill[0].id,
-        sent: false,
-        error: false,
       },
     })
+
+    if (!mailSent) {
+      await context.prisma.emailDelivery.create({
+        data: {
+          id: uuidv4(),
+          created_at: new Date(),
+          updated_at: new Date(),
+          user_id: user.id,
+          email_template_id: templatesThatFulfillPoints[0].id,
+          sent: false,
+          error: false,
+        },
+      })
+    }
   }
 }
