f92238a08b95321924b41c2696bb191ef77d7cd0
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2021-06-02 14:00:27 +0300

    Remove Open Uni alert

--- frontend/components/Home/RegisterCompletion.tsx
+++ frontend/components/Home/RegisterCompletion.tsx
@@ -1,9 +1,6 @@
 import Container from "/components/Container"
 import { H1NoBackground } from "/components/Text/headers"
 import { PropsWithChildren } from "react"
-import { Alert } from "@material-ui/core"
-import RegisterCompletionTranslations from "/translations/register-completion"
-import { useTranslator } from "/util/useTranslator"
 
 interface RegisterCompletionProps {
   title: string
@@ -13,13 +10,11 @@ export default function RegisterCompletion({
   title,
   children,
 }: PropsWithChildren<RegisterCompletionProps>) {
-  const t = useTranslator(RegisterCompletionTranslations)
-
   return (
     <Container>
-      <Alert severity="warning" variant="filled">
+      {/* <Alert severity="warning" variant="filled">
         {t("registrationClosed")}
-      </Alert>
+      </Alert> */}
       <H1NoBackground variant="h1" component="h1" align="center">
         {title}
       </H1NoBackground>
