4a1b52f180f3d36ff0c4f0438f964361fa9f5238
Mikko Pyykkö <mipyykko@gmail.com>
2021-06-03 11:01:13 +0300

    make generate-graphql-types* use correct apollo command

--- frontend/static/types/generated/UserCourseProgressFragment.ts
+++ frontend/static/types/generated/UserCourseProgressFragment.ts
@@ -8,18 +8,18 @@
 // ====================================================
 
 export interface UserCourseProgressFragment_exercise_progress {
-  __typename: "ExerciseProgress"
-  total: number | null
-  exercises: number | null
+  __typename: "ExerciseProgress";
+  total: number | null;
+  exercises: number | null;
 }
 
 export interface UserCourseProgressFragment {
-  __typename: "UserCourseProgress"
-  id: string
-  course_id: string | null
-  max_points: number | null
-  n_points: number | null
-  progress: (any | null)[] | null
-  extra: any | null
-  exercise_progress: UserCourseProgressFragment_exercise_progress | null
+  __typename: "UserCourseProgress";
+  id: string;
+  course_id: string | null;
+  max_points: number | null;
+  n_points: number | null;
+  progress: (any | null)[] | null;
+  extra: any | null;
+  exercise_progress: UserCourseProgressFragment_exercise_progress | null;
 }
