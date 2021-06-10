74df29fa426d0a713be9dc77436be1034d609b56
Mikko Pyykkö <mipyykko@gmail.com>
2021-06-10 14:47:52 +0300

    code style

--- shibbo/types/index.d.ts
+++ shibbo/types/index.d.ts
@@ -1,6 +1,6 @@
 declare module "unfuck-utf8-headers-middleware" {
   function shibbolethCharsetMiddleware(
-    targetHeaders: string[]
+    targetHeaders: string[],
   ): (req: Express.Request, res: Express.Response, next: Function) => void
 
   export = shibbolethCharsetMiddleware
