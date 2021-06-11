f540ae8d2cbc645d661263bffc5a778b5d546f13
Mikko Pyykkö <mipyykko@gmail.com>
2021-06-11 15:49:02 +0300

    added the failure/success page stubs

---
+++ frontend/static/types/generated/ConnectedUser.ts
@@ -0,0 +1,40 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: ConnectedUser
+// ====================================================
+
+export interface ConnectedUser_currentUser_verified_users_organization_organization_translations {
+  __typename: "OrganizationTranslation"
+  language: string
+  name: string
+}
+
+export interface ConnectedUser_currentUser_verified_users_organization {
+  __typename: "Organization"
+  id: string
+  organization_translations: ConnectedUser_currentUser_verified_users_organization_organization_translations[]
+}
+
+export interface ConnectedUser_currentUser_verified_users {
+  __typename: "VerifiedUser"
+  id: string
+  created_at: any | null
+  updated_at: any | null
+  display_name: string | null
+  organization: ConnectedUser_currentUser_verified_users_organization | null
+}
+
+export interface ConnectedUser_currentUser {
+  __typename: "User"
+  id: string
+  upstream_id: number
+  verified_users: ConnectedUser_currentUser_verified_users[]
+}
+
+export interface ConnectedUser {
+  currentUser: ConnectedUser_currentUser | null
+}
