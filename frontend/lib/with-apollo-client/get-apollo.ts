0aac79787298e10056a5939b4451a4cd31e14563
Mikko Pyykkö <mipyykko@gmail.com>
2021-07-22 11:14:07 +0300

    get backend/frontend url from secret

--- frontend/lib/with-apollo-client/get-apollo.ts
+++ frontend/lib/with-apollo-client/get-apollo.ts
@@ -45,7 +45,7 @@ function create(initialState: any, originalAccessToken?: string) {
     uri: cypress
       ? "http://localhost:4001"
       : production
-      ? "https://www.mooc.fi/api/"
+      ? process.env.BACKEND_URL ?? "https://www.mooc.fi/api/"
       : "http://localhost:4000",
     credentials: "same-origin",
     fetch: fetch,
