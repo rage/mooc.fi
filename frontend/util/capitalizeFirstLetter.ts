a6ead797015146bd4f6dd76b002298f5b550d679
Mikko Pyykkö <mipyykko@gmail.com>
2021-07-23 11:43:14 +0300

    fix some more unused stuff

--- frontend/util/capitalizeFirstLetter.ts
+++ frontend/util/capitalizeFirstLetter.ts
@@ -1,4 +1,4 @@
-export default function capitalizeFirstLetter<T extends String>(
+export default function capitalizeFirstLetter<T extends string>(
   string: T,
 ): Capitalize<T> {
   return (string.charAt(0).toUpperCase() + string.slice(1)) as Capitalize<T>
