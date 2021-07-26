c3b47d75682cd73a6d58fc3932aa2d503709cd08
Mikko Pyykkö <mipyykko@gmail.com>
2021-07-26 14:48:12 +0300

    fix shibbo frontend/backend urls

--- shibbo/src/config.ts
+++ shibbo/src/config.ts
@@ -2,16 +2,12 @@ require("dotenv").config()
 
 const isProduction = process.env.NODE_ENV === "production"
 
-console.log("isProduction", isProduction)
-console.log("BACKEND_URL", process.env.BACKEND_URL)
-console.log("FRONTEND_URL", process.env.FRONTEND_URL)
-
 export const HY_ORGANIZATION_SECRET =
   process.env.HY_ORGANIZATION_SECRET || "hy_secret"
 export const HY_ORGANIZATION_ID =
   process.env.HY_ORGANIZATION_ID || "hy_organization"
 export const PORT = process.env.PORT || 5000
 export const BACKEND_URL =
-  process.env.BACKEND_URL ?? !isProduction ? "http://localhost:4000" : ""
+  process.env.BACKEND_URL ?? (!isProduction ? "http://localhost:4000" : "")
 export const FRONTEND_URL =
-  process.env.FRONTEND_URL ?? !isProduction ? "http://localhost:3000" : ""
+  process.env.FRONTEND_URL ?? (!isProduction ? "http://localhost:3000" : "")
