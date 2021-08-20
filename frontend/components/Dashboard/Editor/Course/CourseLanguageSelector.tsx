959f6ce536a5510625e2d40e313ff34118f1fa5a
Mikko Pyykkö <mipyykko@gmail.com>
2021-08-20 20:19:28 +0300

    some graphics to connection buttons

--- frontend/components/Dashboard/Editor/Course/CourseLanguageSelector.tsx
+++ frontend/components/Dashboard/Editor/Course/CourseLanguageSelector.tsx
@@ -31,7 +31,7 @@ const StyledLanguageButton = styled(Button)<ButtonProps>`
     border: 1px solid #83acda;
     color: black;
   }
-  &: disabled {
+  &:disabled {
     box-shadow: 0px 0px 0px rgba(0, 0, 0, 0.5);
     background-color: #354b45;
     color: white;
