8337cdb05df20645680c3c8a64f90c8ada1c3eb5
Mikko Pyykkö <mipyykko@gmail.com>
2021-10-07 12:14:42 +0300

    move strategy/config to own directory

---
+++ shibbo/src/saml/index.ts
@@ -0,0 +1,62 @@
+import { Request } from "express"
+import {
+  MultiSamlStrategy,
+  SamlConfig,
+} from "passport-saml"
+
+import {
+  HAKA_IDP_URL,
+  HY_IDP_URL,
+  MOOCFI_CERTIFICATE,
+} from "../config"
+import {
+  convertObjectKeysToLowerCase,
+  encodeRelayState,
+} from "../util"
+
+const samlProviders: Record<string, string> = {
+  hy: HY_IDP_URL,
+  haka: HAKA_IDP_URL,
+}
+
+export const createSamlStrategy = () => 
+  new MultiSamlStrategy(
+  {
+    passReqToCallback: true,
+    getSamlOptions(req, done) {
+      done (null, createStrategyOptions(req))
+    },
+  },
+  (_req, profile: any, done) => {
+    console.log("got profile", profile)
+    done(
+      null,
+      convertObjectKeysToLowerCase(profile?.attributes)
+    )
+  },
+)
+
+const createStrategyOptions = (req: Request): SamlConfig => {
+  const relayState = encodeRelayState(req)
+  const { provider, action } = req.params
+  const language = req.query.language || req.params.language || "en"
+
+  if (!Object.keys(samlProviders).includes(provider)) {
+    throw new Error(`invalid provider ${provider}`)
+  }
+  
+  return {
+    name: "hy-haka",
+    path: `/callbacks/${provider}/${action}/${language}`,
+    entryPoint: samlProviders[provider],
+    issuer: "https://mooc.fi/sp",
+    cert: MOOCFI_CERTIFICATE,
+    authnRequestBinding: "HTTP-POST",
+    forceAuthn: true,
+    identifierFormat:
+      "urn:oasis:names:tc:SAML:2.0:nameid-format:transient",
+    additionalParams: {
+      RelayState: relayState,
+    },
+  }
+}
--- shibbo/src/server.ts
+++ shibbo/src/server.ts
@@ -5,22 +5,15 @@ import express, {
 } from "express"
 import morgan from "morgan"
 import passport from "passport"
-import { MultiSamlStrategy } from "passport-saml"
 import shibbolethCharsetMiddleware from "unfuck-utf8-headers-middleware"
 
 import {
   DOMAIN,
-  HAKA_IDP_URL,
-  HY_IDP_URL,
-  MOOCFI_CERTIFICATE,
   PORT,
   SHIBBOLETH_HEADERS,
 } from "./config"
 import { callbackHandler } from "./handlers"
-import {
-  convertObjectKeysToLowerCase,
-  encodeRelayState,
-} from "./util"
+import { createSamlStrategy } from "./saml"
 
 const isProduction = process.env.NODE_ENV === "production"
 
@@ -63,11 +56,10 @@ app.use((req, res, next) => {
 app.use(passport.initialize())
 
 
-const providers: Record<string, string> = {
-  hy: HY_IDP_URL,
-  haka: HAKA_IDP_URL,
-}
-
+passport.use(
+  "hy-haka",
+  createSamlStrategy()
+)
 /*passport.serializeUser((user, done) => {
   console.log("serialize", user)
   done(null, user)
@@ -78,41 +70,6 @@ passport.deserializeUser((user, done) => {
   done(null, user as any)
 })*/
 
-passport.use(
-  "hy-haka",
-  new MultiSamlStrategy(
-    {
-      passReqToCallback: true,
-      getSamlOptions(req, done) {
-        const relayState = encodeRelayState(req)
-        const { provider, action } = req.params
-        const language = req.query.language || req.params.language || "en"
-
-        done(null, {
-          name: "multi",
-          path: `/callbacks/${provider}/${action}/${language}`,
-          entryPoint: providers[provider],
-          issuer: "https://mooc.fi/sp",
-          cert: MOOCFI_CERTIFICATE,
-          authnRequestBinding: "HTTP-POST",
-          forceAuthn: true,
-          identifierFormat:
-            "urn:oasis:names:tc:SAML:2.0:nameid-format:transient",
-          additionalParams: {
-            RelayState: relayState,
-          },
-        })
-      },
-    },
-    (_req, profile: any, done) => {
-      console.log("got profile", profile)
-      done(
-        null,
-        convertObjectKeysToLowerCase(profile?.attributes)
-      )
-    },
-  ),
-)
 
 app.get("/:action/:provider", callbackHandler)
 
