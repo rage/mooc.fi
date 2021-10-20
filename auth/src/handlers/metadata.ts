3c65a68e7ae1015cf501c491926b3e88560218a2
Mikko Pyykkö <mipyykko@gmail.com>
2021-10-20 16:22:23 +0300

    try if metadata handler callback works better

--- auth/src/handlers/metadata.ts
+++ auth/src/handlers/metadata.ts
@@ -5,13 +5,19 @@ import { MOOCFI_CERTIFICATE } from "../config"
 
 export function metadataHandler(strategy: MultiSamlStrategy) {
   return (req: Request, res: Response, next: NextFunction) => {
-    res.type("application/xml")
-    res.send(
+    res.status(200).send(
       strategy.generateServiceProviderMetadata(
         req,
         MOOCFI_CERTIFICATE,
         MOOCFI_CERTIFICATE,
-        next,
+        (err, data) => {
+          if (err) {
+            return next()
+          }
+
+          res.set("Content-Type", "application/xml")
+          res.send(data)
+        },
       ),
     )
   }
