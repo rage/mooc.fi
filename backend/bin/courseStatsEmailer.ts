adcc094847488f218a80a3668441a7e527e7e07c
Mikko Pyykkö <mipyykko@gmail.com>
2021-11-11 16:59:40 +0200

    added cronjob

--- backend/bin/courseStatsEmailer.ts
+++ backend/bin/courseStatsEmailer.ts
@@ -1,7 +1,12 @@
 import prisma from "../prisma"
+import sentryLogger from "./lib/logger"
+
+const logger = sentryLogger({ service: "course-stats-emailer" })
 
 const courseStatsEmailer = async () => {
   const subscriptions = await prisma.courseStatsSubscription.findMany({})
+
+  logger.info(`Found ${subscriptions.length} subscriptions`)
 
   for (const subscription of subscriptions) {
     await prisma.emailDelivery.create({
