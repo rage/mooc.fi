77feb0405b443c895f1f453daaf613f70997e37d
Mikko Pyykkö <mipyykko@gmail.com>
2022-01-11 18:43:50 +0200

    run npm ci before auth test

--- bin/run-auth-tests.sh
+++ bin/run-auth-tests.sh
@@ -3,8 +3,14 @@ set -eo pipefail
 
 cd auth
 
+echo ""
+
+npm ci
+
 echo "Running tests"
 
 JEST_JUNIT_OUTPUT_DIR=./coverage/junit/ NODE_ENV=test npm run test -- --ci --coverage --reporters=default --reporters=jest-junit
+
+rm -rf node_modules
 
 cd ..
