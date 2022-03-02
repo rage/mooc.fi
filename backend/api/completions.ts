9a6b15c91c752b8c002099bd29799dfb93cecdc7
Mikko Pyykkö <mipyykko@gmail.com>
2022-03-02 14:50:58 +0200

    fix recheck completion endpoint crashing on nonexisting completions

--- backend/api/completions.ts
+++ backend/api/completions.ts
@@ -323,8 +323,8 @@ export function recheckCompletion({ prisma, logger, knex }: ApiContext) {
         .json({ message: "Completion created", completion: updatedCompletion })
     }
     if (
-      existingCompletion.updated_at?.getTime() !==
-      updatedCompletion.updated_at?.getTime()
+      existingCompletion?.updated_at?.getTime() !==
+      updatedCompletion?.updated_at?.getTime()
     ) {
       return res
         .status(200)
