2295b819e15fa6f5d3b38cd2bd1ecc20c59f5aed
Mikko Pyykkö <mipyykko@gmail.com>
2022-03-04 16:00:36 +0200

    fix extension path

--- backend/migrations/20200811114359_alter-tables-default-values.ts
+++ backend/migrations/20200811114359_alter-tables-default-values.ts
@@ -1,6 +1,7 @@
 import { Knex } from "knex"
 
-import { DEBUG } from "../config"
+import { extensionPath } from "../config"
+import { createUUIDExtension } from "../util/db-functions"
 
 export async function up(knex: Knex): Promise<void> {
   // updated_at
@@ -83,110 +84,88 @@ export async function up(knex: Knex): Promise<void> {
     `ALTER TABLE "verified_user" ALTER COLUMN "updated_at" SET DEFAULT CURRENT_TIMESTAMP;`,
   )
 
-  // id
-  try {
-    await knex.raw(`CREATE SCHEMA IF NOT EXISTS "extensions";`)
-    await knex.raw(
-      `CREATE EXTENSION IF NOT EXISTS "uuid-ossp" SCHEMA "extensions";`,
-    )
-    // commented: hack (?) for parallel
-    /*await knex.raw(`CREATE SCHEMA IF NOT EXISTS "extensions";`)
-    const schema = (await knex.select(knex.raw("current_schema()")))[0]
-      ?.current_schema
-    await knex.raw(`SET SEARCH_PATH TO extensions,"${schema}";`)
-    await knex.raw(
-      `CREATE EXTENSION IF NOT EXISTS "uuid-ossp" SCHEMA "${schema}";`,
-    )
-    await knex.raw(
-      `CREATE EXTENSION IF NOT EXISTS "uuid-ossp" SCHEMA "extensions";`,
-    )*/
-  } catch {
-    Boolean(DEBUG) &&
-      console.warn(
-        "Error creating uuid-ossp extension. Ignore if this didn't fall on next hurdle",
-      )
-  }
+  await createUUIDExtension(knex)
 
   await knex.raw(
-    `ALTER TABLE "completion" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "completion" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "completion_registered" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "completion_registered" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "course" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "course" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "course_alias" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "course_alias" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "course_organization" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "course_organization" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "course_translation" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "course_translation" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "course_variant" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "course_variant" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "email_delivery" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "email_delivery" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "email_template" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "email_template" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "exercise" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "exercise" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "exercise_completion" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "exercise_completion" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "exercise_completion_required_actions" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "exercise_completion_required_actions" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "image" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "image" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "open_university_registration_link" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "open_university_registration_link" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "organization" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "organization" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "organization_translation" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "organization_translation" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "service" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "service" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "study_module" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "study_module" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "study_module_translation" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "study_module_translation" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "user" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "user" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "UserAppDatumConfig" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "UserAppDatumConfig" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "user_course_progress" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "user_course_progress" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "user_course_service_progress" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "user_course_service_progress" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "UserCourseSettings" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "UserCourseSettings" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "user_course_settings_visibility" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "user_course_settings_visibility" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "user_organization" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "user_organization" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
   await knex.raw(
-    `ALTER TABLE "verified_user" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "verified_user" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
 
   // created_at not null
--- backend/migrations/20210610093434_alter-table-ab-studies-ids.ts
+++ backend/migrations/20210610093434_alter-table-ab-studies-ids.ts
@@ -1,26 +1,13 @@
 import { Knex } from "knex"
 
-export async function up(knex: Knex): Promise<void> {
-  try {
-    await knex.raw(`CREATE SCHEMA IF NOT EXISTS "extensions";`)
-    await knex.raw(
-      `CREATE EXTENSION IF NOT EXISTS "uuid-ossp" SCHEMA "extensions";`,
-    )
-  } catch {
-    console.warn(
-      "Error creating uuid-ossp extension. Ignore if this didn't fall on next hurdle",
-    )
-  }
+import { extensionPath } from "../config"
+import { createUUIDExtension } from "../util/db-functions"
 
-  try {
-    // if uuid-ossp already exists, but in another schema
-    await knex.raw(`ALTER EXTENSION "uuid-ossp" SET SCHEMA "extensions";`)
-  } catch {
-    // we can probably ignore this
-  }
+export async function up(knex: Knex): Promise<void> {
+  await createUUIDExtension(knex)
 
   await knex.raw(
-    `ALTER TABLE "ab_study" ALTER COLUMN "id" SET DEFAULT extensions.uuid_generate_v4();`,
+    `ALTER TABLE "ab_study" ALTER COLUMN "id" SET DEFAULT ${extensionPath}uuid_generate_v4();`,
   )
 }
 
