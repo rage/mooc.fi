f22ee79d217bf24b0891e25cffade8b794a7960c
Mikko Pyykkö <mipyykko@gmail.com>
2022-03-04 14:28:26 +0200

    migrate ci config with parallel tests from staging

--- .eslintrc.js
+++ .eslintrc.js
@@ -51,7 +51,7 @@ module.exports = {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
   },
-  extends: ["plugin:jsx-a11y/recommended"],
+  extends: ["plugin:jsx-a11y/recommended", "plugin:@next/next/recommended"],
   settings: {
     react: {
       version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
