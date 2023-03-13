8bdb5adc721f03fb7a9ef91f1757bb5be68c9e4a
Mikko Pyykkö <mipyykko@users.noreply.github.com>
2023-03-13 17:47:20 +0200

    kafka bridge flushProducer timeout error not caught; fixed (#1133)

--- backend/bin/kafkaBridge.ts
+++ backend/bin/kafkaBridge.ts
@@ -83,7 +83,10 @@ app.post("/kafka-bridge/api/v0/event", async (req, res) => {
 
   try {
     producer.produce(topic, null, Buffer.from(JSON.stringify(payload)))
-    flushProducer(1000)
+    flushProducer(1000).catch((err) => {
+      logger.warn(new KafkaError("Flushing the producer failed", err))
+      return res.status(500).json({ error: err.toString() }).send()
+    })
   } catch (e: any) {
     logger.error(new KafkaError("Producing to kafka failed", e))
     return res.status(500).json({ error: e.toString() }).send()
