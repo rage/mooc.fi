0cc37587f5ea3237cdbf0527dc116b91ccdb2146
Mikko Pyykkö <mipyykko@gmail.com>
2023-05-08 17:35:50 +0300

    revert old user points validation to not require original_submission_date

--- backend/bin/kafkaConsumer/userCoursePointsConsumer/validate.ts
+++ backend/bin/kafkaConsumer/userCoursePointsConsumer/validate.ts
@@ -4,11 +4,19 @@ import { MessageYupSchema as UserPointsMessageYupSchema } from "../common/userPo
 
 const CURRENT_MESSAGE_FORMAT_VERSION = 1
 
+const UserPointsExerciseMessageYupSchema = UserPointsMessageYupSchema.shape({
+  original_submission_date: yup.date().when("attempted", {
+    is: true,
+    then: (schema) => schema.required(),
+    otherwise: (schema) => schema.nullable().notRequired(),
+  }),
+})
+
 export const MessageYupSchema = yup.object().shape({
   timestamp: yup.date().required(),
   user_id: yup.number().required(),
   course_id: yup.string().length(36).required(),
-  exercises: yup.array(UserPointsMessageYupSchema).required(),
+  exercises: yup.array(UserPointsExerciseMessageYupSchema).required(),
   message_format_version: yup
     .number()
     .min(CURRENT_MESSAGE_FORMAT_VERSION)
