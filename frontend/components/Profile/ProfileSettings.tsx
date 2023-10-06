aa1acc0ecad17a553e55f86fc795d8fecae0344a
Antti Leinonen <antti.leinonen@helsinki.fi>
2023-10-06 15:26:25 +0300

    Fix profile in old layout

--- frontend/components/Profile/ProfileSettings.tsx
+++ frontend/components/Profile/ProfileSettings.tsx
@@ -1,16 +1,24 @@
 import CTALink from "../NewLayout/Common/CTALink"
+import useIsNew from "/hooks/useIsNew"
 import { useTranslator } from "/hooks/useTranslator"
 import ProfileTranslations from "/translations/profile"
 
 const ProfileSettings = () => {
   const t = useTranslator(ProfileTranslations)
+  const isNew = useIsNew()
 
   return (
     <>
       <p>{t("TMCSettingsInfo")}</p>
-      <CTALink href="https://tmc.mooc.fi/participants/me" target="_blank">
-        {t("TMCProfileLink")}
-      </CTALink>
+      {isNew ? (
+        <CTALink href="https://tmc.mooc.fi/participants/me" target="_blank">
+          {t("TMCProfileLink")}
+        </CTALink>
+      ) : (
+        <a href="https://tmc.mooc.fi/participants/me" target="_blank">
+          {t("TMCProfileLink")}
+        </a>
+      )}
     </>
   )
 }
