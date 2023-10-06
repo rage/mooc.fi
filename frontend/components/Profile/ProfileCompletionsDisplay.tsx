fc2e6e723903a99e4c818091cabacaa2ae3dbeef
Antti Leinonen <antti.leinonen@helsinki.fi>
2023-10-06 15:35:10 +0300

    Link to new versions in profile

--- frontend/components/Profile/ProfileCompletionsDisplay.tsx
+++ frontend/components/Profile/ProfileCompletionsDisplay.tsx
@@ -2,6 +2,7 @@ import { Typography } from "@mui/material"
 
 import { FormSubmitButton } from "/components/Buttons/FormSubmitButton"
 import { CompletionListItem } from "/components/Home/Completions"
+import useIsNew from "/hooks/useIsNew"
 import { useTranslator } from "/hooks/useTranslator"
 import ProfileTranslations from "/translations/profile"
 import { completionHasCourse } from "/util/guards"
@@ -15,6 +16,7 @@ interface CompletionsProps {
 const ProfileCompletionsDisplay = (props: CompletionsProps) => {
   const { completions } = props
   const t = useTranslator(ProfileTranslations)
+  const isNew = useIsNew()
 
   return (
     <>
@@ -27,7 +29,11 @@ const ProfileCompletionsDisplay = (props: CompletionsProps) => {
       {completions.length === 0 && (
         <Typography>{t("nocompletionsText")}</Typography>
       )}
-      <FormSubmitButton href="/profile/completions" variant="text" fullWidth>
+      <FormSubmitButton
+        href={isNew ? "/_new/profile/completions" : "/profile/completions"}
+        variant="text"
+        fullWidth
+      >
         {t("seeCompletions")}
       </FormSubmitButton>
     </>
--- frontend/components/Profile/ProfilePointsDisplay.tsx
+++ frontend/components/Profile/ProfilePointsDisplay.tsx
@@ -4,6 +4,7 @@ import { FormSubmitButton } from "/components/Buttons/FormSubmitButton"
 import ErrorMessage from "/components/ErrorMessage"
 import Spinner from "/components/Spinner"
 import PointsListGrid from "/components/User/Points/PointsListGrid"
+import useIsNew from "/hooks/useIsNew"
 import { useTranslator } from "/hooks/useTranslator"
 import ProfileTranslations from "/translations/profile"
 
@@ -12,12 +13,13 @@ import { CurrentUserProgressesDocument } from "/graphql/generated"
 const ProfilePointsDisplay = () => {
   const { data, error, loading } = useQuery(CurrentUserProgressesDocument)
   const t = useTranslator(ProfileTranslations)
+  const isNew = useIsNew()
 
   if (loading) {
     return <Spinner />
   }
 
-  if (error || !data) {
+  if (error ?? !data) {
     return <ErrorMessage />
   }
 
@@ -28,7 +30,11 @@ const ProfilePointsDisplay = () => {
   return (
     <>
       <PointsListGrid data={data} showOnlyTen />
-      <FormSubmitButton href="/profile/points" variant="text" fullWidth>
+      <FormSubmitButton
+        href={isNew ? "/_new/profile/points" : "/profile/points"}
+        variant="text"
+        fullWidth
+      >
         {t("seePoints")}
       </FormSubmitButton>
     </>
