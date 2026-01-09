ba3dacb7e1d6e2533fdee15f6f6549b584178068
Henrik Nygren <nygrenh@gmail.com>
2026-01-09 11:14:16 +0200

    Stop using getIntialProps

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,10 @@
 import { useInsertionEffect, useMemo } from "react"
 
 import { DefaultSeo } from "next-seo"
-import type { AppContext, AppProps, NextWebVitalsMetric } from "next/app"
+import type { AppProps, NextWebVitalsMetric } from "next/app"
 import Head from "next/head"
 import Script from "next/script"
 
-import { NormalizedCacheObject } from "@apollo/client"
 import { CssBaseline } from "@mui/material"
 import { ThemeProvider } from "@mui/material/styles"
 
@@ -13,37 +12,19 @@ import DynamicLayout from "/components/DynamicLayout"
 import AppContextProvider from "/contexts/AppContextProvider"
 import { LoginStateProvider } from "/contexts/LoginStateContext"
 import useAlternateLanguage from "/hooks/useAlternateLanguage"
+import { useAuth } from "/hooks/useAuth"
 import useIsOld from "/hooks/useIsOld"
 import { useScrollToHash } from "/hooks/useScrollToHash"
 import useSeoConfig from "/hooks/useSeoConfig"
 import useThemeWithLocale from "/hooks/useThemeWithLocale"
-import { getAccessToken, isAdmin, isSignedIn } from "/lib/authentication"
 import withApolloClient from "/lib/with-apollo-client"
-import { createEmotionSsr } from "/src/createEmotionSsr"
 
-import { UserDetailedFieldsFragment } from "/graphql/generated"
-
-const { withAppEmotionCache, augmentDocumentWithEmotionCache } =
-  createEmotionSsr({
-    key: "moocfi",
-  })
-
-export { augmentDocumentWithEmotionCache }
-
-interface MyPageProps {
-  signedIn?: boolean
-  admin?: boolean
-  currentUser?: UserDetailedFieldsFragment
-  accessToken?: string
-}
-
-interface MyAppProps extends AppProps<MyPageProps> {
+interface MyAppProps extends AppProps {
   deviceType?: string
-  apolloState?: NormalizedCacheObject
 }
 
 export function MyApp({ Component, pageProps, deviceType }: MyAppProps) {
-  const { signedIn, admin, currentUser } = pageProps ?? {}
+  const { signedIn, admin, currentUser } = useAuth()
 
   useInsertionEffect(() => {
     const jssStyles = document?.querySelector("#jss-server-side")
@@ -61,9 +42,9 @@ export function MyApp({ Component, pageProps, deviceType }: MyAppProps) {
 
   const loginStateContextValue = useMemo(
     () => ({
-      loggedIn: signedIn ?? false,
-      admin: admin ?? false,
-      currentUser: currentUser,
+      loggedIn: signedIn,
+      admin: admin,
+      currentUser: currentUser ?? undefined,
     }),
     [signedIn, admin, currentUser],
   )
@@ -103,43 +84,9 @@ export function MyApp({ Component, pageProps, deviceType }: MyAppProps) {
   )
 }
 
-// @ts-ignore: initialProps
-const originalGetInitialProps = MyApp.getInitialProps
-
-MyApp.getInitialProps = async (props: AppContext) => {
-  const { ctx, Component } = props
-
-  const signedIn = isSignedIn(ctx)
-  const admin = signedIn && isAdmin(ctx)
-  const accessToken = getAccessToken(ctx)
-
-  let originalProps: any = {}
-
-  if (originalGetInitialProps) {
-    originalProps = (await originalGetInitialProps(props)) ?? {}
-  }
-  if (!originalProps?.pageProps) {
-    originalProps.pageProps = {}
-  }
-  if (signedIn && Component.getInitialProps) {
-    const originalComponentProps = (await Component.getInitialProps(ctx)) ?? {}
-    Object.assign(originalProps.pageProps, originalComponentProps)
-  }
-
-  return {
-    ...originalProps,
-    pageProps: {
-      ...originalProps.pageProps,
-      signedIn,
-      admin,
-      accessToken,
-    },
-  }
-}
-
 // @ts-ignore: silence for now
 export function reportWebVitals(metric: NextWebVitalsMetric) {
   // console.log(metric)
 }
 
-export default withAppEmotionCache(withApolloClient(MyApp))
+export default withApolloClient(MyApp)
